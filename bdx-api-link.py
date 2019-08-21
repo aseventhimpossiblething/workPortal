aaa59c21d9d22cfd52cb66449dc566966e1585de
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-08-21 09:43:37 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -1,11 +1,9 @@
-#heroku create myapp --buildpack heroku/python
-# an object of WSGI application
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)   # Flask constructor
 
-# A decorator used to tells the application
-# which URL is associated function
+
 @app.route('/')
 def hello():
     return "Hello le monde"
@@ -28,10 +26,5 @@ def hollb():
 
 
 if __name__=='__main__':
-#if '__name__'=='__main__':
-#if __bdx-api-link__=='__main__':
-#if '__bdx-api-link__'=='__main__':
-#if 'bdx-api-link'=='__main__':
-#if bdx-api-link=='__main__':
     app.run()
 
