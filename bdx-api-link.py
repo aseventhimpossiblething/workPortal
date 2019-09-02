5915b74d622aa5fff752bbd6987e37c2744572ff
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-02 12:31:14 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -24,7 +24,7 @@ def upload():
     #os.chdir(r'/app/Sheets')
     print("**************************flag 2******************************")
     #print(request.files['sheet'].save(os.path.join('/app/Sheets/sheet',request.files['sheet'].filename)))
-    print(os.getcwd())
+    #print(os.getcwd())
     print(os.path.join('/app/Sheets',request.files['sheet'].filename))
     print(request.files['sheet'].filename)
     print(request.files['sheet'])
