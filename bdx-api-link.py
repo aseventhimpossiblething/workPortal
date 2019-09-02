c1a0a09718c8e52bf2f2f9c53c44610c9a144738
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-02 17:59:11 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -19,7 +19,7 @@ def hello():
 @app.route('/upload', methods=['POST','GET'])
 def upload():
     fileHandler.fileHandler()
-    return render_template('csstemplate.css')
+    return f'finish
       
 @app.route('/css')
 def hollb():
