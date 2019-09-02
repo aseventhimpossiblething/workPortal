5ca35c4dd9a9970dc70d31b35f91f0078cd018aa
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-02 17:03:26 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -1,8 +1,10 @@
 print("Checkpoint 1-before imports")
 import BidOpAssist
+import fileHandler
+from flask import Flask, render_template, request
 import os
 import psycopg2
-from flask import Flask, render_template, request
+
 app = Flask(__name__)   # Flask constructor
 print("Checkpoint 2-before Database connection")
 conn = psycopg2.connect("dbname='dcect276ul8asc' user='ffsezxsqjvacnw' host='ec2-54-83-9-36.compute-1.amazonaws.com' password='657c149f7aac22520e75d72bddb9a16c76e60ac324fb4358f9f579ac1c2619d4'")
@@ -16,44 +18,7 @@ def hello():
 
 @app.route('/upload', methods=['POST','GET'])
 def upload():
-    print("********************************flag 1************************************************")
-    print("request.files______:   ",request.files)
-    print("********************************flag 2************************************************")
-    print("request.files['sheet']______:    ",request.files['sheet'])
-    print("********************************flag 3*************************************************")
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-    #print("request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))")
-    #request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))
-    #os.chdir(r'/app/Sheets')
-    print("*********************************flag 4***********************************************")
-    print("os.getcwd()_____: ",os.getcwd())
-    #print(request.files['sheet'].save(os.path.join('/app/Sheets/sheet',request.files['sheet'].filename)))
-    print("********************************flag 5************************************************")
-    print("os.listdir()____:",os.listdir())
-    print("********************************flag 6************************************************")
-    os.chdir('/app/Sheets')
-    print("os.chdir(/Sheets)____:")
-    print("********************************flag 7************************************************")
-    print("os.getcwd()_____: ",os.getcwd())
-    print("********************************flag 8************************************************")
-    print("os.listdir()____:",os.listdir())
-    print("********************************flag 9************************************************")
-    print("request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))")
-    request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))
-    print("********************************flag 10************************************************")
-    print("os.getcwd()____:",os.getcwd)
-    print("********************************flag 11************************************************")
-    print("os.listdir()____:",os.listdir())
-    print("********************************flag 12************************************************")
-    print("os.path.join('/app/Sheets',request.files['sheet'].filename))_____:",os.path.join('/app/Sheets',request.files['sheet'].filename))
-    print("********************************flag 13************************************************")
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-    print("********************************flag 14************************************************")
-    print("request.files['sheet']______:    ",request.files['sheet'])
-    print("********************************flag 15************************************************")
-    print("request.files______:    ",request.files)
-    print("**************************flag 16******************************************************")
-    return request.method
+   
    
 
 @app.route('/css')
