70717fdeaae0e3d19d98ebeac0465d0f1d4aae7b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-02 16:59:17 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -1,1 +1,76 @@
+def fileHandler():
+    print("********************************flag 1************************************************")
 
+    print("request.files______:   ",request.files)
+
+    print("********************************flag 2************************************************")
+
+    print("request.files['sheet']______:    ",request.files['sheet'])
+
+    print("********************************flag 3*************************************************")
+
+    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
+
+    #print("request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))")
+
+    #request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))
+
+    #os.chdir(r'/app/Sheets')
+
+    print("*********************************flag 4***********************************************")
+
+    print("os.getcwd()_____: ",os.getcwd())
+
+    #print(request.files['sheet'].save(os.path.join('/app/Sheets/sheet',request.files['sheet'].filename)))
+
+    print("********************************flag 5************************************************")
+
+    print("os.listdir()____:",os.listdir())
+
+    print("********************************flag 6************************************************")
+
+    os.chdir('/app/Sheets')
+
+    print("os.chdir(/Sheets)____:")
+
+    print("********************************flag 7************************************************")
+
+    print("os.getcwd()_____: ",os.getcwd())
+
+    print("********************************flag 8************************************************")
+
+    print("os.listdir()____:",os.listdir())
+
+    print("********************************flag 9************************************************")
+
+    print("request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))")
+
+    request.files['sheet'].save(os.path.join('/app/Sheets',request.files['sheet'].filename))
+
+    print("********************************flag 10************************************************")
+
+    print("os.getcwd()____:",os.getcwd)
+
+    print("********************************flag 11************************************************")
+
+    print("os.listdir()____:",os.listdir())
+
+    print("********************************flag 12************************************************")
+
+    print("os.path.join('/app/Sheets',request.files['sheet'].filename))_____:",os.path.join('/app/Sheets',request.files['sheet'].filename))
+
+    print("********************************flag 13************************************************")
+
+    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
+
+    print("********************************flag 14************************************************")
+
+    print("request.files['sheet']______:    ",request.files['sheet'])
+
+    print("********************************flag 15************************************************")
+
+    print("request.files______:    ",request.files)
+
+    print("**************************flag 16******************************************************")
+
+    return request.method
