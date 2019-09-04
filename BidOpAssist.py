92dfe2f1034b208df12a99d8e9151176a0682fa7
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-04 15:10:27 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -13,8 +13,8 @@ print(open('sample.txt').read())
 #except 
 #readSample=open('sample.txt').read()
 #print(readSample)
-incomingSheet=open('Bid_OpExperiment.xlsx')
-pandas.read_excel(incomingSheet)
+#incomingSheet=open('Bid_OpExperiment.xlsx')
+#pandas.read_excel(incomingSheet)
 
 #incomingSheet=open('Bid_OpExperiment.xlsx')
 #SHeetRead=incomingSheet.read()
