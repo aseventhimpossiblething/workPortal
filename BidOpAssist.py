a1cb19b2bbae353840653f4bb24ba3531aae970d
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-04 12:46:45 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -4,8 +4,6 @@ def BidOpAssist(x,y,z):
     print("***BidOpAssist Running********")
     print(x,y,z)    
 BidOpAssist("BidOpAssist is Running as expected","Second Slot","Third Slot")
-print(os.getcwd())
-print(os.listdir())
 print(os.chdir('Sheets'))
 print(os.getcwd())
 print(os.listdir())
