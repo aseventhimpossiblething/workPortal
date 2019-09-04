2cc78d06b7247e5577cd268411570aa1bf8abdd8
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-04 12:52:45 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -7,7 +7,7 @@ BidOpAssist("BidOpAssist is Running as expected","Second Slot","Third Slot")
 print(os.chdir('Sheets'))
 print(os.getcwd())
 print(os.listdir())
-#open('Bid_OpExperiments.xlsx')
+open('Bid_OpExperiment.xlsx')
 
 
 
