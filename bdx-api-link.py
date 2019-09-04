24715f4807c0467cc7a82925d67bcb1bb5b0b147
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-04 00:33:53 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -18,6 +18,8 @@ print("attempting to create tables")
 #conn.cursor().execute("CREATE TABLE DocumentSubmissions("Documents")")
 conn.cursor().execute("CREATE TABLE storage (user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, email VARCHAR (355) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)")
 conn.cursor().close()
+conn.commit()
+conn.close
 print("check for tables")
 print(conn.cursor().execute("SELECT * FROM pg_stat_user_tables"))
 
