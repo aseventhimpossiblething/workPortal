e4b69d6aa528c191d1e42b6d444f4e29c023b10c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-04 13:28:24 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -5,7 +5,7 @@ def BidOpAssist(x,y,z):
     print("***BidOpAssist Running********")
     print(x,y,z)    
 BidOpAssist("BidOpAssist is Running as expected","Second Slot","Third Slot")
-print(os.chdir('Sheets'))
+os.chdir('Sheets')
 #pandas.read_excel()
 #incomingSheet=open('Bid_OpExperiment.xlsx')
 #SHeetRead=incomingSheet.read()
