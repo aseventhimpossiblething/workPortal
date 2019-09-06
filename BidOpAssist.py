84c756fec71b479ba2dd0eaba493761ce5c356ac
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-06 10:09:47 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -7,7 +7,10 @@ from sklearn.ensemble import RandomForestRegressor
 os.chdir('Sheets')
 
 #important Variables
+#Sheet_to_Analyse=
 Dimension_Predicted='New CPC'
+ExampleSheetName='Machine.xlsx'
+
 ModelCol1=['Campaign','Ad group','Keyword','New CPC','Max. CPC','Avg. CPC','Cost','Clicks','Conversions','Impr.','CTR']
 ModelCol2=['Cost / conv.','Impr. (Top) %','Impr. (Abs. Top) %','Search impr. share','Search lost IS (rank)','Quality Score','Match type']
 ModelColumns=ModelCol1+ModelCol2
@@ -18,13 +21,13 @@ X_Sheet_Analysis="Empty"
 print("************************patterns 1**********************************************")
 print("Pattern_inputModel",Pattern_inputModel)
 print("Pattern_New_CPC",Pattern_New_CPC)
-#print("X_Sheet_Analysis",X_Sheet_Analysis)
+print("X_Sheet_Analysis",X_Sheet_Analysis)
 #print("",)
 
 print("************************patterns 2**********************************************")
 
 def PrepModel():      
-    PatternSheet=open('Machine.xlsx', 'rb')
+    PatternSheet=open(ExampleSheetName, 'rb')
     Pattern_no_Frame=pandas.read_excel(PatternSheet)
     PatternSheetFramed=pandas.DataFrame(Pattern_no_Frame, columns=ModelColumns).fillna(0)
     global Pattern_New_CPC
@@ -34,7 +37,20 @@ def PrepModel():
 PrepModel()    
 print("Pattern_inputModel",Pattern_inputModel)
 print("Pattern_New_CPC",Pattern_New_CPC)    
+
+
+
+def Analysis():
+    Sheet_To_Be_analysed=open('To_Test_Machine_Goog.xlsx','rb')
+    FramedSheet_To_Be_Analysed=pandas.DataFrame(pandas.read_excel(Sheet_To_Be_analysed), columns=ModelColumns).fillna(0)
+    #the below are for testing only
+    global X_Sheet_Analysis
+    X_Sheet_Analysis=FramedSheet_To_Be_Analysed.drop(ColumnsToClear_for_Analysis, axis=1)
+    #Y_Sheet_Analysis=FramedSheet_To_Be_Analysed[Dimension_Predicted]
     
+
+
+
 """
 
 #Sheet_To_Be_analysed=open('To_Test_Machine_Goog.xlsx','rb')
@@ -47,18 +63,7 @@ BidOpAssist("BidOpAssist tester","Second Slot","Third Slot")
 
 
 
-#this is the function that prepares the Pattern Sheet
 
-#print('pattern prep running**************************')
-PatternSheet=open('Machine.xlsx', 'rb')
-Pattern_no_Frame=pandas.read_excel(PatternSheet)
-PatternSheetFramed=pandas.DataFrame(Pattern_no_Frame, columns=ModelColumns).fillna(0)
-Pattern_New_CPC=PatternSheetFramed[Dimension_Predicted]
-Pattern_inputModel=PatternSheetFramed.drop(ColumnsToClear_for_Analysis, axis=1)
-#print('******************* Pattern_New_CPC ***************************')
-#print(Pattern_New_CPC)
-#print('******************* Pattern_inputModel ***************************')
-#print(Pattern_inputModel)
 
 
 
@@ -73,7 +78,7 @@ Y_Sheet_Analysis=FramedSheet_To_Be_Analysed[Dimension_Predicted]
 
 
 taughtModel=RandomForestRegressor(n_estimators=25).fit(Pattern_inputModel,Pattern_New_CPC)
-#outputArr=taughtModel.predict(X_Sheet_Analysis)
+outputArr=taughtModel.predict(X_Sheet_Analysis)
 #print(outputArr)
 #taughtModel=RandomForestRegressor(n_estimators=25).fit(no_Col_Head_Pattern_inputModel,no_Col_Pattern_New_CPC)
 print("fini")
