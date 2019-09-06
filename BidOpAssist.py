2891a7cb6b7913192fb77c7377fb634fd19a9e62
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-06 08:03:31 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -6,11 +6,12 @@ import pandas
 from sklearn.ensemble import RandomForestRegressor
 os.chdir('Sheets')
 
+Dimension_Predicted='New CPC'
 ModelCol1=['Campaign','Ad group','Keyword','New CPC','Max. CPC','Avg. CPC','Cost','Clicks','Conversions','Impr.','CTR']
 ModelCol2=['Cost / conv.','Impr. (Top) %','Impr. (Abs. Top) %','Search impr. share','Search lost IS (rank)','Quality Score','Match type']
 ModelColumns=ModelCol1+ModelCol2
-ColumnsToClear_for_Analysis=['New CPC','Campaign','Ad group','Keyword','Match type']
-Dimension_Predicted='New CPC'
+ColumnsToClear_for_Analysis=[Dimension_Predicted,'Campaign','Ad group','Keyword','Match type']
+
 
 #PatternSheet=open('Machine.xlsx', 'rb')
 #Sheet_To_Be_analysed=open('To_Test_Machine_Goog.xlsx','rb')
@@ -37,6 +38,7 @@ print(Pattern_inputModel)
 
 Sheet_To_Be_analysed=open('To_Test_Machine_Goog.xlsx','rb')
 FramedSheet_To_Be_Analysed=pandas.DataFrame(pandas.read_excel(Sheet_To_Be_analysed), columns=ModelColumns).fillna(0)
+#the below are for testing only
 X_Sheet_Analysis=FramedSheet_To_Be_Analysed.drop(ColumnsToClear_for_Analysis, axis=1)
 Y_Sheet_Analysis=FramedSheet_To_Be_Analysed[Dimension_Predicted]
 
@@ -45,6 +47,7 @@ Y_Sheet_Analysis=FramedSheet_To_Be_Analysed[Dimension_Predicted]
 
 
 taughtModel=RandomForestRegressor(n_estimators=25).fit(Pattern_inputModel,Pattern_New_CPC)
+taughtModel.predict(X_Sheet_Analysis)
 #taughtModel=RandomForestRegressor(n_estimators=25).fit(no_Col_Head_Pattern_inputModel,no_Col_Pattern_New_CPC)
 print("fin")
 
