9b60ce606bc98b44ea8249bf385135fb1504cc13
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-12 08:10:07 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -48,7 +48,7 @@ print("check for tables")
 def hello():
     return "Hello le monde"
 
-@app.route('/upload', methods=['POST','GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
     fileHandler.fileHandler()
     return fileHandler.fileHandler()
