9e9d3638b49a2941bc62c88e1b96ed0bd8e2645a
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-12 08:19:58 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -88,7 +88,7 @@ def fileHandler():
     print(request.form)
     #print("reques.data")
     #print(request.data)
-    request.files['sheet'].getattr
+    
     #print("request.files.size()")
     #print(request.files.size())
     #print("request.files['sheet'].size()")
