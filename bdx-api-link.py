86712f641e9b3676988a1ccf976c5226af3da297
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-17 14:07:00 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -46,7 +46,7 @@ app = Flask(__name__)   # Flask constructor
 @app.route('/')
 def index():
     indexContent=Markup('<a href="https://www.google.com">"Potentially links"</a><br>
-    <a href="https://www.google.com">"Potentially links"</a>')
+        <a href="https://www.google.com">"Potentially links"</a>')
     return render_template('DefaultTemplate.html',content=indexContent)
 @app.route('/BidOPUpload', methods=['POST','GET'])
 def BidOPUpload():
