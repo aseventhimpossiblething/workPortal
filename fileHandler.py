8fd580e4d937a81c0d7f6c2a830a1b8212003fcf
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-18 10:25:45 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -1,6 +1,7 @@
 import BidOpAssist
 from flask import Flask, render_template, request
 import glob
+import Markup
 import os
 import psycopg2
 import pandas
@@ -152,29 +153,9 @@ def CommListFileHandler():
     print("********************************CommListFileHandler() flag 11************************************************")
 
     print("os.listdir()____:",os.listdir())
-
-    print("********************************CommListFileHandler() flag 12************************************************")
-
-    #print("os.path.join('/app/Sheets/CommunityUpdates/currentCommunities',request.files['Communities'].filename))_____:\
-    #",os.path.join('/app/Sheets/CommunityUpdates/currentCommunities',request.files['Communities'].filename))
+    HTMLoutput=Markup(<p></p>)
     
-    #print("os.path.join('/app/Sheets/CommunityUpdates/Google/currentGoogle',request.files['currentGoogle'].filename))_____:\
-    #",os.path.join('/app/Sheets/CommunityUpdates/currentCommunities/Google/currentGoogle',request.files['currentGoogle'].filename))
-
-    print("********************************CommListFileHandler() flag 13************************************************")
-
-    #print("request.files['Communities'].filename_______:     ",request.files['Communities'].filename)
-
-    print("********************************CommListFileHandler() flag 14************************************************")
-
-    #print("request.files['Communities']______:    ",request.files['Communities'])
-
-    print("********************************CommListFileHandler() flag 15************************************************")
-
-   
-    print("**************************CommListFileHandler() flag 16******************************************************")
-    
-    toscrn = "done"
+    toscrn = HTMLoutput
     print("**************************CommListFileHandler() flag 17******************************************************")
 
     
@@ -182,150 +163,6 @@ def CommListFileHandler():
 
 
 
-def CurrentGoogleFileHandler():
-    print("********************************CurrentGoogleFileHandler() flag 1************************************************")
-
-    print("request.files______:   ",request.files)
-
-    print("********************************CurrentGoogleFileHandler() flag 2************************************************")
-
-    print("request.files['sheet']______:    ",request.files['sheet'])
-
-    print("********************************CurrentGoogleFileHandler() flag 3*************************************************")
-
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-
-    print("*********************************CurrentGoogleFileHandler() flag 4***********************************************")
-
-    print("os.getcwd()_____: ",os.getcwd())
-
-    print("********************************CurrentGoogleFileHandler() flag 5************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentGoogleFileHandler() flag 6************************************************")
-
-    os.chdir('/app/Sheets')
-
-    print("os.chdir(/Sheets)____:")
-
-    print("********************************CurrentGoogleFileHandler() flag 7************************************************")
-
-    print("os.getcwd()_____: ",os.getcwd())
-
-    print("********************************CurrentGoogleFileHandler() flag 8************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentGoogleFileHandler() flag 9************************************************")
-
-    request.files['sheet'].save(request.files['sheet'].filename)
-
-    print("********************************CurrentGoogleFileHandler() flag 10************************************************")
-
-    print("os.getcwd()____:",os.getcwd)
-
-    print("********************************CurrentGoogleFileHandler() flag 11************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentGoogleFileHandler() flag 12************************************************")
-
-    print("os.path.join('/app/Sheets',request.files['sheet'].filename))_____:",os.path.join('/app/Sheets',request.files['sheet'].filename))
-
-    print("********************************CurrentGoogleFileHandler() flag 13************************************************")
-
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-
-    print("********************************CurrentGoogleFileHandler()) flag 14************************************************")
-
-    print("request.files['sheet']______:    ",request.files['sheet'])
-
-    print("********************************CurrentGoogleFileHandler() flag 15************************************************")
-
-    print("request.files______:    ",request.files)
-
-    print("**************************CurrentGoogleFileHandler() flag 16******************************************************")
-    
-    toscrn = "done"
-    print("**************************CurrentGoogleFileHandler() flag 17******************************************************")
-
-    
-    return toscrn
-
-
-
-def CurrentBingFileHandler():
-    print("********************************CurrentBingFileHandler() flag 1************************************************")
-
-    print("request.files______:   ",request.files)
-
-    print("********************************CurrentBingFileHandler() flag 2************************************************")
-
-    print("request.files['sheet']______:    ",request.files['sheet'])
-
-    print("********************************CurrentBingFileHandler() flag 3*************************************************")
-
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-
-    print("*********************************CurrentBingFileHandler() flag 4***********************************************")
-
-    print("os.getcwd()_____: ",os.getcwd())
-
-    print("********************************CurrentBingFileHandler() flag 5************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentBingFileHandler() flag 6************************************************")
-
-    os.chdir('/app/Sheets')
-
-    print("os.chdir(/Sheets)____:")
-
-    print("********************************CurrentBingFileHandler() flag 7************************************************")
-
-    print("os.getcwd()_____: ",os.getcwd())
-
-    print("********************************CurrentBingFileHandler() flag 8************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentBingFileHandler() flag 9************************************************")
-
-    request.files['sheet'].save(request.files['sheet'].filename)
-
-    print("********************************CurrentBingFileHandler() flag 10************************************************")
-
-    print("os.getcwd()____:",os.getcwd)
-
-    print("********************************CurrentBingFileHandler() flag 11************************************************")
-
-    print("os.listdir()____:",os.listdir())
-
-    print("********************************CurrentBingFileHandler() flag 12************************************************")
-
-    print("os.path.join('/app/Sheets',request.files['sheet'].filename))_____:",os.path.join('/app/Sheets',request.files['sheet'].filename))
-
-    print("********************************CurrentBingFileHandler() flag 13************************************************")
-
-    print("request.files['sheet'].filename_______:     ",request.files['sheet'].filename)
-
-    print("********************************CurrentBingFileHandler() flag 14************************************************")
-
-    print("request.files['sheet']______:    ",request.files['sheet'])
-
-    print("********************************CurrentBingFileHandler() flag 15************************************************")
-
-    print("request.files______:    ",request.files)
-
-    print("**************************CurrentBingFileHandler() flag 16******************************************************")
-    
-    toscrn = "done"
-    print("**************************CurrentBingFileHandler() flag 17******************************************************")
-    
-
-    
-    return toscrn
 
 
     
