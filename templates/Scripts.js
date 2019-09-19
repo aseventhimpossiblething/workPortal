9375d78f57ad8ddb407946ffadc60e1b1abbaabd
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-19 10:12:58 -0500

    Update Scripts.js

--- templates/Scripts.js
+++ templates/Scripts.js
@@ -1,9 +1,11 @@
 function ValidateForm(){
 //console.log(document.forms["CommForm"]["Communitites"].value)
 //console.log(console.log(document.forms["CommForm"]["Communitites"].value))  
-console.log("ExternalJS is being read");
-alert(" ExternalJS is being read")  
-//console.log(document.forms["CommForm"]["Communitites"].value)
+//console.log("ExternalJS is being read");
+//alert(" ExternalJS is being read")  
+alert(document.forms["CommForm"]["Communitites"].value)
+console.log(document.forms["CommForm"]["Communitites"].value)  
+  
 }
 
 //window.onload=ValidateForm();
