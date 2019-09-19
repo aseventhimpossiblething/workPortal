e5aac382cb9a85cde52ffc693f192434282665cc
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-19 11:36:45 -0500

    Update Scripts.js

--- templates/Scripts.js
+++ templates/Scripts.js
@@ -1,6 +1,7 @@
 function ValidateForm(){
 n=1  
-if(document.forms["CommForm"]["Communitites"].value){  
+if(document.getElemantby[frmID]){
+   
 //document.forms["CommForm"]["Communitites"].value;
 //console.log(console.log(document.forms["CommForm"]["Communitites"].value))  
 //console.log("ExternalJS is being read");
@@ -9,7 +10,10 @@ if(document.forms["CommForm"]["Communitites"].value){
 //console.log(document.forms["CommForm"]["Communitites"].value)
 alert("form halted")  
 return false;}  
+}
 
+function testing(){
+alert("")
 }
 
 //window.onload=ValidateForm();
