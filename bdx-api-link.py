ff16b5c37cdf14bd76bb93776448fe973e8ea77d
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-20 07:21:30 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -76,9 +76,9 @@ def CommunitiesUploads():
 @app.route('/CommunityFileHander', methods=['POST','GET'])
 def CommunityFileHandling():
     try: return "Try worked"
-    Except:  return "uh oh.. I bet the file was to large. check the file size and type If you still have trouble, Let me know when I come in ---Mike"
+    Except: return "uh oh.. I bet the file was to large. check the file size and type If you still have trouble, Let me know when I come in ---Mike"
     
-    return fileHandler.uploadTryCatch()
+    #return fileHandler.uploadTryCatch()
     
     
     
