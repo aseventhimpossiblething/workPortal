5e69545600fc7e9c0b7db55c67f9e2f4eba7a5fb
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-20 15:02:43 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -76,9 +76,11 @@ def CommunitiesUploads():
 @app.route('/CommunityFileHander', methods=['POST','GET'])
 def CommunityFileHandling():
     try:
+        CommunityUpdatesProcess.initialCommUpdatProcess()
         return fileHandler.CommListFileHandler()
     #return fileHandler.CommListFileHandler()
     except: 
+        CommunityUpdatesProcess.initialCommUpdatProcess()
         return Markup("Excepted")
     #"""    
     #"uh oh.. I bet the file was to large. check the file size and type If you still have trouble, Let me know when I come in ---Mike"
