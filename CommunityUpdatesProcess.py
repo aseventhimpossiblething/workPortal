9f3a5e71e2e0551df3c2bb24ff4e81aa44526e65
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-24 11:05:57 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -10,22 +10,23 @@ import psycopg2
 
 
 def initialCommUpdatProcess():
-  print("***************************************************")
+  print("**********************initialCommUpdatProcess()*****************************")
   os.chdir('/app/Sheets/CommunityUpdates/currentCommunities')
   WorkingCommunities1=open('WorkingCommunities')
   print(pandas.read_excel('WorkingCommunities'))
   print(pandas.read_excel('WorkingCommunities').drop([0,1,2,3,4]))
   
    
-  print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Community Id']))
-  print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Community Id','Bacon Shreds']))
+  #print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Community Id']))
+  #print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Community Id','Bacon Shreds']))
   
   
-  
+  """
   print(os.listdir())
   print(open('WorkingCommunities'))
   print(WorkingCommunities1)
   #print(WorkingCommunities2)
+  """
   
   
   
