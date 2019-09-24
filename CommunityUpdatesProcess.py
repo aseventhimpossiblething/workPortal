118f30aded6e77a58c791a3c7a97558898fbc767
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-24 17:31:11 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -25,7 +25,7 @@ def initialCommUpdatProcess():
     'Community Id','Community Name','City','State','Zip','Market ID','Market Name'])
   
   
-  return list(WorkingCommunities)
+  return WorkingCommunities[0]
 
     
   
