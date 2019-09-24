6530c17b58a1af7e30d1634fb03f63642b191bcd
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-24 14:26:10 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -23,7 +23,7 @@ def initialCommUpdatProcess():
   #pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Brand Name','Division Id','Division Name','Community Id','Community Name','City','State','Zip','Market ID','Market Bame'])
   WorkingCommunities=pandas.DataFrame(WorkingCommunities, columns=['Builder Name','Brand Name','Division Id','Division Name',\
     'Community Id','Community Name','City','State','Zip','Market ID','Market Name'])
-  #print(list(WorkingCommunities.head()))
+  print(WorkingCommunities)
   return WorkingCommunities
 
     
