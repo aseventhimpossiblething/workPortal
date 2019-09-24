4c6e5ad74c3cb6c72de14e90c63437a83ccc3260
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-24 12:51:24 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -12,22 +12,19 @@ import psycopg2
 def initialCommUpdatProcess():
   print("**********************initialCommUpdatProcess()*****************************")
   os.chdir('/app/Sheets/CommunityUpdates/currentCommunities')
-  #WorkingCommunities=open('WorkingCommunities')
   WorkingCommunities=pandas.read_excel('WorkingCommunities').drop([0,1,2,3])
   print(WorkingCommunities)
   WorkingCommunities.columns=WorkingCommunities.iloc[0]
   print(WorkingCommunities)
   WorkingCommunities=WorkingCommunities.drop([0])
+  print(WorkingCommunities)
   #print(pandas.read_excel('WorkingCommunities').drop([0,1,2]))
   #pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Brand Name','Division Id','Division Name','Community Id','Community Name','City','State','Zip','Market ID','Market Bame'])
   pandas.DataFrame(WorkingCommunities, columns=['Builder Name','Brand Name','Division Id','Division Name','Community Id','Community Name','City','State','Zip','Market ID','Market Bame'])
  
    
    
-  #print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Brand Name','Division Id','Division Name','Community Id','Community Name','City','State','Zip','Market ID','Market Bame']))
-  #print(pandas.DataFrame(pandas.read_excel('WorkingCommunities'),columns=['Builder Name','Community Id','Bacon Shreds']))
-  
-  
+
   
   
   
