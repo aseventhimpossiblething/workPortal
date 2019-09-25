21d806e6023de0392df718b4f8b5d263d0cd8c60
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-25 11:16:09 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -137,7 +137,7 @@ def CommListFileHandler():
            
     CommunityUpdatesProcess.initialCommUpdatProcess() 
     #print(CommunityUpdatesProcess.initialCommUpdatProcess())    
-    HTMLoutput=CommunityUpdatesProcess.CommunityColTitles
+    HTMLoutput=<html>CommunityUpdatesProcess.CommunityColTitles</html>
     print(CommunityUpdatesProcess.CommunityColTitles)
     print(CommunityUpdatesProcess.CommunityRow1) 
     print(CommunityUpdatesProcess.CommunityRow2)
