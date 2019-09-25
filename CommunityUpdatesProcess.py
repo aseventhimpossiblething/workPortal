b2b8a47c23c7087c193545a4a9d10019e5501101
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-09-25 01:09:33 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -33,8 +33,8 @@ def initialCommUpdatProcess():
   print(WorkingCommunities[['Builder Name','Brand Name']])
   print("**********************WorkingCommunities.values*****************************")
   print(WorkingCommunities.values)
-  print("**********************WorkingCommunities.iloc(5)*****************************")
-  print(WorkingCommunities.iloc[5])
+  print("**********************WorkingCommunities.iloc(5).transpose()*****************************")
+  print(WorkingCommunities.iloc[5].transpose())
   
   
   return "finished"
