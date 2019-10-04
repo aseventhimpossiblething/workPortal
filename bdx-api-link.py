80cbaf97258958d03d9d2752d2b53030ef3f8c33
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-04 07:58:47 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -55,7 +55,8 @@ def spitOut():
     return "Spit out, Spit Out"
 spitOut()
 print("attempt to run spitOut")
-spitOut.delay()
+print("celery failed")
+#spitOut.delay()
 #print(spitOut().delay())
 #add.delay(a=2,b=2)
 #print("add(8,9).delay()",add(8,9).delay())
