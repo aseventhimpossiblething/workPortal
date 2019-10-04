02d23ce7fdfe6f20e0fa54f00d0be7fc3b9888d2
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-04 07:09:04 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -52,7 +52,7 @@ celery = Celery('myapp', broker='amqp://guest@localhost//')
 @celery.task
 def add(x, y):
     return x + y
-add(3,4).delay()
+add.delay(2,2)
 #print("add(8,9).delay()",add(8,9).delay())
 #print("add(8,9).ready()",add(8,9).ready())
 #print("add(8,9).result()",add(8,9).result())
