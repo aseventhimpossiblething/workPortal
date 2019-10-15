e8c4fec8e3c1fe4ff5e7e26eb3f33a4569080f60
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-15 13:08:40 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -68,7 +68,7 @@ def CelTest():
   print("CelTest is testing")
   return ("Returned Value from CelTest")
 print(CelTest.delay())
-CelTest.delay().result()
+CelTest.delay().state()
 #print(CelTest)
 
 
