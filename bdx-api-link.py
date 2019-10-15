644aa581d5308d04b5374599a30ba48e9808ef49
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-15 14:04:13 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -68,7 +68,7 @@ celery.conf.update(app.config)
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
-    TaskBase = celery.Task
+    TaskBase = celery.task
     class ContextTask(TaskBase):
         abstract = True
         def __call__(self, *args, **kwargs):
