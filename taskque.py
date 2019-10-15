d3a2e1697121b07e7096a6a92301cb0e8330126e
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-15 14:37:51 -0500

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -8,8 +8,19 @@ import fileHandler
 import redis
 import os
 from redis import Redis
+form celery import Celery
 
 
+app = Celery('taskque', broker=os.environ['REDIS_URL'])
+def initiLjov():
+  print("tasque File Running initiJoV")
+initijoV()  
+  
+
+#app.config['CELERY_BROKER_URL'] = os.environ['REDIS_URL']
+#app.config['CELERY_RESULT_BACKEND'] = os.environ['REDIS_URL']
+#celery = Celery(app.name, broker=os.environ['REDIS_URL'])
+#celery.conf.update(app.config)
 
 #print("Task loaded")
 """
