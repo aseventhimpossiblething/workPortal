2f68a14f960d39e357d4b1b526aa8a2e7192362c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-15 12:15:12 -0500

    Update task.py

--- task.py
+++ task.py
@@ -1,18 +1,7 @@
-import CommunityUpdatesProcess
-import glob
-import numpy
-import scipy
-import pandas
-import BidOpAssist
-import fileHandler
-import redis
-from redis import Redis
-from flask import Flask
-from celery import Celery
 
 
 
-
+"""
 print(os.environ['REDIS_URL'])
 
 app.config['CELERY_BROKER_URL'] = os.environ['REDIS_URL']
@@ -21,4 +10,5 @@ app.config['CELERY_RESULT_BACKEND'] = os.environ['REDIS_URL']
 
 celery = Celery(app.name, broker=os.environ['REDIS_URL'])
 celery.conf.update(app.config)
+"""
 
