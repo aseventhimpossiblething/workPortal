9551192502646dcec08fa5ba8a2a372dcc5a51f9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-16 14:30:52 -0500

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -47,6 +47,7 @@ def initiLjoV():
   print("tasque File Running initiJoV")
   Rval="return value"
   return Rval
+redis.Redis().client_getname()
 #run_initiLjoV=initiLjoV.delay()
 
 #print("initiLjoV State 1",run_initiLjoV.state)
