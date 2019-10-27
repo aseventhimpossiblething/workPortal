895ecf5b7766dd26c52a8326cbdc8cc9bea824a5
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-27 12:10:56 -0500

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -14,8 +14,9 @@ from flask import Flask
 #the_redis=os.environ.get("REDIS_URL")
 the_redis=redis.from_url(os.environ.get("REDIS_URL"))
 print("REDIS_URL",REDIS_URL)
-print('os.environ.get("REDIS_URL")',os.environ.get("REDIS_URL"))
+
 print('redis.from_url(os.environ.get("REDIS_URL"))',redis.from_url(os.environ.get("REDIS_URL")))
+print('os.environ.get("REDIS_URL")',os.environ.get("REDIS_URL"))
 """
 
 def make_celery(app):
