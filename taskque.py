cd41856059e47c569b332e67cd0df1494c02c2d2
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-10-27 14:17:08 -0500

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -17,12 +17,12 @@ from flask import Flask
 
     
 
-the_redis=os.environ.get("REDIS_URL")
-#the_redis=redis.from_url(os.environ.get("REDIS_URL"))
+#the_redis=os.environ.get("REDIS_URL")
+the_redis=redis.from_url(os.environ.get("REDIS_URL"))
 #print("REDIS_URL",REDIS_URL)
 
-print('current - setting 1 - os.environ.get("REDIS_URL")',os.environ.get("REDIS_URL"))
-print('setting 2 - redis.from_url(os.environ.get("REDIS_URL"))__:-->',redis.from_url(os.environ.get("REDIS_URL")))
+print('setting 1 - os.environ.get("REDIS_URL")',os.environ.get("REDIS_URL"))
+print('current - setting 2 - redis.from_url(os.environ.get("REDIS_URL"))__:-->',redis.from_url(os.environ.get("REDIS_URL")))
 
 """
 
