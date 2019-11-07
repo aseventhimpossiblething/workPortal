a64c35311f27a37b4b7c89f14cb3113e39edad53
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-11-07 15:26:28 -0600

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -25,7 +25,7 @@ from flask import Flask
 #print('setting 1 - os.environ.get("REDIS_URL")',os.environ.get("REDIS_URL"))
 #print('current - setting 2 - redis.from_url(os.environ.get("REDIS_URL"))__:-->',redis.from_url(os.environ.get("REDIS_URL")))
 
-#cel=Celery("Tasks",the_redis)
+cel=Celery("Tasks",the_redis)
 #cel=Celery("Tasks", broker=the_redis)
 #cel=Celery("Tasks",CELERY_BROKER_URL=the_redis)
 @cel.task()
