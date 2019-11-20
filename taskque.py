5dbe418e9e56381e64a04d088faeb1cde3746a5d
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-11-20 13:31:27 -0600

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -59,7 +59,7 @@ def initiLjoV():
 run_initiLjoV=initiLjoV.apply_async()
 taskId=run_initiLjoV.task_id
 print("taskId.....This is the task ID............",taskId)
-#result=run_initiLjoV.result
+result=run_initiLjoV.result
 #print(result)
 
 
