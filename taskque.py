ef157b7d43fa41411c9e12c1c0ecb331901c25b5
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-02 14:00:36 -0600

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -90,7 +90,7 @@ print("____________________________+++++++++++++++++(4)++++++++++++++___________
 
 #AsyncResult(taskId).status()
 #result.state
-result.state()
+#result.state()
 result.status
 result.status()
 
