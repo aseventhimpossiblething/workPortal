c8c5744a06cea246bbf691fb01e8cd51f5102991
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-06 09:03:06 -0600

    Update taskque.py

--- taskque.py
+++ taskque.py
@@ -105,10 +105,9 @@ print('run_initiLjoV.ready()...This seems to work? ...',run_initiLjoV.ready())
 #result.status()
 
 
-
-print("run_initiLjoV.state".........,run_initiLjoV.state)
+print("run_initiLjoV.state.........",run_initiLjoV.state)
 #run_initiLjoV.status()
-print("run_initiLjoV.status"........,run_initiLjoV.status)
+print("run_initiLjoV.status........",run_initiLjoV.status)
 #run_initiLjoV.result()
 #run_initiLjoV.result
 
