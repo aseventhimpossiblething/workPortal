e87d4b2beeebf80595ad56cabeb3fd4250e6fce9
Connor Osborn <connor@cdosborn.com>
2019-12-07 17:32:54 -0500

    wip

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -8,84 +8,42 @@ import fileHandler
 import redis
 import os
 import taskque
-#from redis import Redis
-#the_redis=redis.from_url(os.environ.get("REDIS_URL"))
-
-#print('redis.from_url(os.environ.get("REDIS_URL"))',the_redis)
-
-#print("os.environ['REDIS_URL']",os.environ['REDIS_URL'])
+from celery import Celery
 
 from flask import Flask, Markup, render_template, request
-from celery import Celery
 
 
 import psycopg2
 from sklearn.ensemble import RandomForestRegressor
 app = Flask(__name__)
-#print("THIS SHOWS AS app <<<<<  in main doc",app)
-#print(app)
-#redis.Redis().client_getname()
 
+# def print_num_redis_connections():
+#     c = redis.Redis(host='redis')
+#     print("# of redis clients", len(c.client_list()))
 
-#hgzlzxkufcvhaspkldjfh v;hgpwsodjbh;akdswryg
-#kajsnbvlajne f[lbksnv;lrgnj
-
-
-
-#DATABASE_URL = os.environ['DATABASE_URL']
-
-
-#application = Flask(__name__)   # Flask constructor
-#app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
-
-
-
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-#conn = psycopg2.connect("dbname='dcect276ul8asc' user='ffsezxsqjvacnw' host='ec2-54-83-9-36.compute-1.amazonaws.com' password='657c149f7aac22520e75d72bddb9a16c76e60ac324fb4358f9f579ac1c2619d4'")
-
-#conn.cursor().execute("SELECT * FROM information_schema.tables ")
-#conn.cursor().execute("CREATE TABLE newTable (id int,Data text)")
-
-#print("run __: ",conn.cursor().execute("SELECT * FROM dcect276ul8asc"))
-#print('run conn.cursor().execute("SELECT * FROM information_schema.tables")___:', conn.cursor().execute("SELECT * FROM information_schema.tables"))
-#print(conn.cursor().execute("SELECT * FROM pg_stat_user_tables"))
-
-#print("attempting to create tables")
-#conn.cursor().execute("CREATE TABLE DocumentSubmissions("Documents")")
-#conn.cursor().execute("CREATE TABLE storage (user_id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, email VARCHAR (355) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)")
-
-#conn.cursor().close()
-#conn.commit()
-#print(conn.cursor().execute("SELECT * FROM pg_stat_user_tables"))
-#conn.close
-
-#print(conn.cursor().execute("SELECT * FROM pg_stat_user_tables"))
-#{{CommonTag}}-{{pagetitle}}
-
-
-
-
-
-
-
+# print_num_redis_connections()
 
 
 CommonTagAll=Markup('<a href="https://bdx-api-link.herokuapp.com/">BDX Paid Search Portal</a>')
 
+@app.route('/num_of_redis_clients')
+def num_redis_clients():
+    c = redis.Redis(host='redis', max_connections=1)
+    print("# of redis clients", len(c.client_list()))
+    return str(len(c.client_list()))
+
 @app.route('/test')
 def testasynch():
-    j=2+2
-    j=str(j)
-    return j
-
-
+    from taskque import test
+    r = test()
+    return str(r)
 
 @app.route('/css')
 def styleSheet1():
     return render_template('csstemplate.css')
 
 @app.route('/Scripts')
-def Scripts():    
+def Scripts():
     return render_template('Scripts.js')
 
 @app.route('/')
@@ -119,19 +77,15 @@ def CommunityFileHandling():
     #CommunityUpdatesProcess.initialCommUpdatProcess()
     print("++++++++++++++++++   filehandler Running   ++++++++++++++++++++++")
     fileHandler.CommListFileHandler()
-    
+
     return OnPageIterationOfComupdate()
     """
     try:
         return fileHandler.CommListFileHandler()
-   
-    except: 
+
+    except:
         return Markup("Files Prohobited")
-    """    
-   
-
-
+    """
 
 if __name__=='__main__':
     app.run()
-
--- taskque.py
+++ taskque.py
@@ -5,8 +5,6 @@ reliable redis/celery cmds
 voir les log persistant
 Separer les contenue static es dynamique
 """
-print("this is the master branch tasque..............................................................")
-
 
 import CommunityUpdatesProcess
 import glob
@@ -18,143 +16,33 @@ import fileHandler
 import redis
 import os
 from redis import Redis
-from celery import Celery
 from flask import Flask
-from celery.result import AsyncResult
-from celery.result import ResultBase
 
-#print("++++++++++++++++++++++++++++++++++++",app.name())
-#print("++++++++++++++++++++++++++++++++++++",app.name)
-print("____________________________+++++++++++++++++(1)++++++++++++++_________________________________")
+from huey import RedisHuey
 
+pool = redis.BlockingConnectionPool(host="redis", max_connections=5, timeout=None)
+huey = RedisHuey(name='app', connection_pool=pool)
 
-#print(Redis)    
-#the_redis='redis://localhost:6379/0'
-the_redis=os.environ.get("REDIS_URL")
-print('REDIS_URL.....................',the_redis)
-
-
-print("____________________________+++++++++++++++++(2)++++++++++++++_________________________________")
-
-
-
-
-
-cel=Celery("Tasks", broker=the_redis, backend=the_redis)
-
-@cel.task()
-def zfunc():
-    print("--------------PRINTED FROM IN ZFUNC")
+@huey.task()
+def test():
+    from time import sleep
+    # sleep(1)
+    print("All done")
     return 42
-zfunc.apply_async()
 
-#zfunc.apply_async(countdown=100)
+# from celery import Celery
+# from celery.result import AsyncResult
+# from celery.result import ResultBase
 
+# the_redis=os.environ.get("REDIS_URL")
 
-print("____________________________+++++++++++++++++(3)++++++++++++++_________________________________")
+# cel=Celery("Tasks", broker=the_redis, backend=the_redis)
+# cel.conf.broker_pool_limit = 0
+# cel.conf.redis_max_connections = 1
 
-
-
-#zfunc()
-#print("zfunc.delay()------",zfunc.delay())
-#Zfunc.delay()
-#Zfunc.apply_async()
-@cel.task()
-def initiLjoV():
-  print("tasque File Running initiJoV")
-  Rval="return value"
-  return Rval
-#initiLjoV.delay()
-#initiLjoV.apply_async()
-#run_initiLjoV=initiLjoV.delay()
-run_initiLjoV=initiLjoV.apply_async()
-taskId=run_initiLjoV.task_id
-print("taskId.....This is the task ID............",taskId)
-#run_initiLjoV=initiLjoV.apply_async()
-result=run_initiLjoV.result
-#run_initiLjoV=initiLjoV.apply_async()
-print('printed result____________',result)
-print(AsyncResult(taskId))
-
-
-
-
-
-print("____________________________+++++++++++++++++(4)++++++++++++++_________________________________")
-
-
-
-
-
-
-
-
-
-
-
-#run_initiLjoV.result.state()
-print('run_initiLjoV.result...',run_initiLjoV.result)
-
-print("AsyncResult(taskId).ready()...",AsyncResult(taskId).ready())
-print('run_initiLjoV.ready()...This seems to work? ...',run_initiLjoV.ready())
-
-#AsyncResult(taskId).status()
-#result.state
-#result.state()
-#result.status
-#result.status()
-
-
-print("run_initiLjoV.state.........",run_initiLjoV.state)
-#run_initiLjoV.status()
-print("run_initiLjoV.status........",run_initiLjoV.status)
-#run_initiLjoV.result()
-#run_initiLjoV.result
-
-#AsyncResult(taskId).status()
-
-
-
-"""
-initiLjoV.delay().state
-initiLjoV.delay().status
-initiLjoV.delay().result
-"""
-
-
-#redis.Redis().client_getname()
-#run_initiLjoV=initiLjoV.delay()
-
-
-"""
-print('redis.Redis()',redis.Redis())
-print('redis.Redis',redis.Redis)
-print('redis',redis)
-"""
-
-#print('This is the client id',redis.Redis().client_id)
-
-
-
-
-#print("initiLjoV State 1",run_initiLjoV.state)
-#redis.Redis().flushdb()
-#print("ended redis")
-#print(redis.scan())
-
-#print("initiLjoV State 2",run_initiLjoV.state)
-"""
-print("initiLjoV State 3",run_initiLjoV.state)
-print("initiLjoV State 4",run_initiLjoV.state)
-print("initiLjoV State 5",run_initiLjoV.state)
-print("initiLjoV State 6",run_initiLjoV.state)
-print("initiLjoV State 7",run_initiLjoV.state)
-print("initiLjoV State 8",run_initiLjoV.state)
-"""
-#print("result",run_initiLjoV.result)
-
-
-
-
-
-
+# @cel.task()
+# def test():
+#     from time import sleep
+#     sleep(2)
+#     print("All done")
+#     return 42
