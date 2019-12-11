6b21cceacdbf4583893e62b61f36d5650e40d309
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-11 11:26:16 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -105,10 +105,12 @@ def LoadCommunities(WorkingCommunities,checkword1,checkword2,checkword3):
     print("Load Communities cannot run...............",IsCommValid)
   return IsCommValid    
 
+print("C -E-L-E-R-Y- -T-A-S-K--S-H-O-U-L-D- -R-U-N- -N-O-W")
 @cel.task()
 def celOFfgroun():
   print("This exists of Tasque but should run on celery------------")
 celOFfgroun()  
+print("C -E-L-E-R-Y- -T-A-S-K--S-H-O-U-L-D- -R-U-N- -N-O-W")
  
 
     
