cf6e219318cf742263b303a48898b16c32b3151c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-12 07:28:47 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -123,7 +123,7 @@ def initialCommUpdatProcess():
   print('os.listdir()')
   print(os.listdir())
   print("taskqueFileAsynchLoad()")
-  taskque.FileAsynchLoad.apply_async(WorkingGoogle,'/app/Sheets/CommunityUpdates/Google/currentGoogle')
+  taskque.FileAsynchLoad.apply_async('WorkingGoogle','/app/Sheets/CommunityUpdates/Google/currentGoogle')
   """
   WorkingGoogle=pandas.read_excel('WorkingGoogle')
   print('WorkingGoogle')
