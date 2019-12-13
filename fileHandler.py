bf0e41b41657901b46fe5b338993502cc067ffc1
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-13 09:31:11 -0600

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -166,7 +166,7 @@ def CommListFileHandler():
     CommunityUpdatesProcess.initialCommUpdatProcess()
     if CommunityUpdatesProcess.IsCommValid!="Valid":
      return CommunityUpdatesProcess.IsCommValid
-    elif:CommunityUpdatesProcess.IsGoogleValid!="Valid"
+    elif CommunityUpdatesProcess.IsGoogleValid!="Valid":
      return CommunityUpdatesProcess.IsGoogleValid           
     HTMLoutput="This will be 3 modules  Modules as follows  Module 1: 3 links to the Community, Google, and Bing upload outputs Module 2:Google Outputs link1, Google KWs all match types. Link 2 google Adds Ad Types A+b and all Match types "    
     toscrn = HTMLoutput
