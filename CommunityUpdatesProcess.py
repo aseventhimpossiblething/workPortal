7b374cf22bf158f8285fafb9cfef99f5fececa7c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-17 11:46:47 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -127,7 +127,7 @@ def initialCommUpdatProcess():
  WorkingGoogleEOF=WorkingGoogle()    
  WorkingBingEOF=WorkingBing()
  
- print(WorkingGoogleEOF['Final URL'])
+ print(WorkingGoogleEOF['Final URL'].head())
  
  
  
