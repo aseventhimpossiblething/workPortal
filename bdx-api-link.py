9f49330b39024d153526652a6924f3593bddca2d
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-17 09:09:01 -0600

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -51,7 +51,7 @@ app = Flask(__name__)
 
 print("initial directory",os.getcwd())
 print("contents of current directory",os.listdir())
-os.chdir("/app/Sheets/communityUpdates")
+os.chdir("/app/Sheets/CommunityUpdates")
 print("current directory",os.getcwd())
 print("contents of current directory",os.listdir())
 
