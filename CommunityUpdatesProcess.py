be000521f6163ab550ca6e6bcc8d735f1c4a7dfc
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-17 08:20:04 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -148,7 +148,7 @@ def initialCommUpdatProcess():
  print("contents.....",os.listdir())
 
  print("It wrote now open and read")
- TheSamplefile=open('TheSampleText','r')
+ print(open('TheSampleText','r'))
  #print(pandas.read_csv(TheSamplefile))
  #print(pandas.read_csv('TheSampleText.txt'))
  #print(pandas.read_csv(TheSamplefile))
