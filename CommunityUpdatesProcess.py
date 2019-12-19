1feb4e6ca10446ac87f3623ddc713304bbc6a67f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-19 17:27:36 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -147,7 +147,7 @@ def initialCommUpdatProcess():
   NewGoogle=[];
   NewBing=[];
   while count < 5:
-  while count < checkby.count():
+  #while count < checkby.count():
    if str(checkin1).find(str(checkby[count]))<0:
     NewGoogle.append(str(checkby[count]));
    if str(checkin2).find(str(checkby[count]))<0:  
