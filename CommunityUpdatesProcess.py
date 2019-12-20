87f4ed3ff69f11ad74ac8e84b8a85ff8d496c856
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-20 09:50:41 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -149,7 +149,7 @@ def initialCommUpdatProcess():
   print(checkby2)
   print("checkby[224]",checkby[224])
   print("str(checkin1).find(checkby2[224])",checkby[224])
-  print("checkby[223]",checkby[225])
+  print("checkby[225]",checkby[225])
   print("str(checkin1).find(checkby2[225])",checkby[225])
   print("checkby[224]",checkby[226])
   print("str(checkin1).find(checkby2[226])",checkby[226])
@@ -158,7 +158,7 @@ def initialCommUpdatProcess():
   
   NewGoogle=[];
   NewBing=[];
-  while count < 225:
+  while count < 224:
   #while count < checkby.count():
    if str(checkin1).find(checkby2[count])<0:
     NewGoogle.append(checkby[count]);
