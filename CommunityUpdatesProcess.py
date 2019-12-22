ec37a823bf0e4f638c1b2a4228a24fbbf91ca7ce
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-22 09:58:41 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -153,14 +153,14 @@ def initialCommUpdatProcess():
   (International Homes,A5),(International Homes,B5),(Clayton,A5);'
   count=5;
   count2=0;
-  
+  """
   print("FrameToBeFiltered['Brand Name']",FrameToBeFiltered['Brand Name'])
   print("FilterString.find(str(FrameToBeFiltered['Brand Name']))",FilterString.find(str(FrameToBeFiltered['Brand Name'])))
   #FilterString.find(str(FrameToBeFiltered['Brand Name'][1]))
   print("FrameToBeFiltered['Brand Name'][1]",FrameToBeFiltered['Brand Name'][1])
   print("FilterString.find(str(FrameToBeFiltered['Brand Name'][1]))",FilterString.find(str(FrameToBeFiltered['Brand Name'][1])))
   #FilterString.find(str(FrameToBeFiltered['Brand Name'][1]))
-  
+  """
   """
   #print("#1 Variables initiated Begin first while loop")
   while count < FrameToBeFiltered['Builder Name'].count():
