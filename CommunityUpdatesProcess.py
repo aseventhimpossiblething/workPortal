2731faa3622c083a498237b260499bf4a75fed0c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-22 05:08:49 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -152,15 +152,19 @@ def initialCommUpdatProcess():
   count=5
   while count < FrameToBeFiltered['Builde Name'].count():
    if FilterString.find(FrameToBfiltered['Builder Name'][count])<0:
-    ResultOfAcolFilter.append([count])
-    print("Filter out: ",FrameToBfiltered[count]," Row",count)
+    if str(ResultOfAcolFilter).find(str(count))<0:
+     ResultOfAcolFilter.append([count])
+     print("Filter out: ",FrameToBfiltered[count]," Row",count)
    if FilterString.find(ResultOfAcolFilter['Brand Name'][count])<0:
-    ResultOfBcolFilter.append([count])
-    print("Filter out: ",ResultOfAcolFilter[count]," Row",count)
+    if str(ResultOfAcolFilter).find(str(count))<0:
+     ResultOfAcolFilter.append([count])
+     print("Filter out: ",ResultOfAcolFilter[count]," Row",count)
    if FilterString.find(ResultOfBcolFilter['Community Id'][count])<0:
-    ResultOfQcolFilter.append([count])
-    print("Filter out: ",ResultOfAcolFilter[count]," Row",count) 
-  print("End Filter")  
+    if str(ResultOfAcolFilter).find(str(count))<0:
+     ResultOfAcolFilter.append([count])
+     print("Filter out: ",ResultOfAcolFilter[count]," Row",count) 
+  print("End Filter") 
+  return ResultOfAcolFilter
  filterNonParticipators(WorkingCommunities)   
      
   
