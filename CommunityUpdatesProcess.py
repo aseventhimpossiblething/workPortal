bd43de5dddc65121225cf1acbc3171c611df0ab9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-22 18:40:40 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -156,6 +156,8 @@ def initialCommUpdatProcess():
   
   print("numpy.array(FrameToBeFiltered['Brand Name'])",numpy.array(FrameToBeFiltered['Brand Name']))
   
+  print("numpy.array(FrameToBeFiltered['Brand Name'])[1]",numpy.array(FrameToBeFiltered['Brand Name'])[1])
+  
   print("FrameToBeFiltered['Brand Name']",FrameToBeFiltered['Brand Name'])
   print("FrameToBeFiltered['Brand Name'][1]",FrameToBeFiltered['Brand Name'][1])
   #print("FilterString.find(str(FrameToBeFiltered['Brand Name']))",FilterString.find(str(FrameToBeFiltered['Brand Name'])))
