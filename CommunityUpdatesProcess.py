c0c7b2af11088867b98a26311aeed4fa6f1bfc5c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-25 09:50:23 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -179,7 +179,8 @@ def initialCommUpdatProcess():
   """
 
   #print("#1 Variables initiated Begin first while loop")
-  while count < len(numpy.array(FrameToBeFiltered['Brand Name'])):
+  while count < 10:
+  #while count < len(numpy.array(FrameToBeFiltered['Brand Name'])):
    #print("#2 first while loop running if FilterString.find(FrameToBeFiltered['Builder Name'][count])<0:") 
    if FilterString.find(str(numpy.array(FrameToBeFiltered['Brand Name'])[[count]]))<0:
     #print(count,"#3 First if of first loop running ")
