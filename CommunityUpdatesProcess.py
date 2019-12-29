a075aaa5f9a06737584dc419a5f34de7e9bf8100
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2019-12-28 18:48:51 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -217,12 +217,13 @@ def initialCommUpdatProcess():
   while count2 < 3: 
   #while count2 < len(ResultOfAcolFilter):
    print("ResultOfAcolFilter",ResultOfAcolFilter)
-   print("ResultOfAcolFilter[count-8]",ResultOfAcolFilter[count-8])
+   #print("ResultOfAcolFilter[count2]",ResultOfAcolFilter[count2])
    print("ResultOfAcolFilter[count2]",ResultOfAcolFilter[count2])
    #FilteredFrame=FilteredFrame.iloc(ResultOfAcolFilter[count2]).drop()
    ResultOfAcolFilter[count2] 
    print("#13 second loop of filter amount of members len(ResultOfAcolFilter) ",len(ResultOfAcolFilter))
    count+=1;
+   count2+=1;
    
   print("End Filter") 
   
