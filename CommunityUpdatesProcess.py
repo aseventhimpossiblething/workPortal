a2f9bb9b940bd17381e08f05d819bf169cdfbfed
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-03 20:27:23 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -139,7 +139,7 @@ def communityCheck(checkby,checkin,Name):
  
 expDataCol=[];
 def KeywordGen(NewDataFrame,MatchType,SearchChan):
- print("google Keyword_conv DF ",NewDataFrame['Community Name'])
+ #print("google Keyword_conv DF ",NewDataFrame['Community Name'])
  #numberofLoops=NewDataFrame.count();
  MatchType=MatchType.upper();
  SearchChan=SearchChan.lower();
