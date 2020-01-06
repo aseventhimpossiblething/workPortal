245dbc9049a48b07b141383f78db14c32a18868f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-06 07:35:03 -0600

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -106,13 +106,11 @@ def CommUpdateDisplay():
     print("read_storeRequest ",read_storeRequest)     
     print("from start of route CommunityUpdatesProcess.IsCommUpdateRunning ",CommunityUpdatesProcess.IsCommUpdateRunning)
     print("________________________________________________________________exp ",read_storeRequest1," : ",read_storeRequest2)
-   
-"""
     if read_storeRequest1==read_storeRequest2:
-     return "Are they equal? They Should be"  
+     return "Are they equal? They Should be solid page"  
     if read_storeRequest1!=read_storeRequest2:
      return '<meta http-equiv="refresh" content="60"><html>"LOADING..... need reload code"</html>'  
-""" 
+
 
 """
 @app.route('/dnlde')
