831f763fc17c5116e155fcf6c8f27d7f9f72d286
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-09 15:12:26 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -344,6 +344,7 @@ def initialCommUpdatProcess():
  
  KeywordGen(NewGoogle,"sbmm","google")
  KeywordGen(NewGoogle,"sb","google")
+ KeywordGen(NewGoogle,"sx","google")
  """        
  KeywordGen(NewGoogle,"sx","google")
  KeywordGen(NewBing,"sbmm","bing")
