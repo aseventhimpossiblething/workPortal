3eac90e3a29e448b1a3246bbfcc99087dad23739
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-11 13:37:26 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -229,7 +229,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    Path2A.append("New Homes")
    print('_______________________6_________________________')
    print("State ",NewDataFrame['State'][count].lower(),)
-   print("City ",NewDataFrame['City'][count]replace(" ","-").lower())
+   print("City ",NewDataFrame['City'][count].replace(" ","-").lower())
    Final_URL.append("https://www.newhomesource.com/community/"+NewDataFrame['State'][count].lower()+NewDataFrame['City'][count].replace(" ","-").lower())
    print('_______________________7_____________________________________________________________________________')
    print("https://www.newhomesource.com/community/"+NewDataFrame['State'][count].lower()+"/"+NewDataFrame['City'][count].replace(" ","-").lower())
