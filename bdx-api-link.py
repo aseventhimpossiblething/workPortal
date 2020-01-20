186cc6db076989526aa86b6ef28bc24be52673c3
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-20 10:19:11 -0600

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -125,7 +125,7 @@ def GoogleKWSX():
 @app.route('/GoogleAdSBMMA')
 def GoogleASBMM():
  print(date.today())        
- TodaysDate="EL-Dia-De-Hoy"        
+ TodaysDate="EL-Dia-De-Hoy"+date.today()+".xlsx"        
  return send_file("/app/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionA/GoogleAdsVersionABMM/DefaultSheet.xlsx",\
                  #attachment_filename="GoogleAdSBMMA.xlsx")
                   attachment_filename=TodaysDate)  
