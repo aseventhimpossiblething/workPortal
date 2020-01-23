d8d1f581d3f639babafc683b0fe26e6ea85f5f74
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-23 15:40:29 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -113,6 +113,9 @@ def filterNonParticipators(FrameToBeFiltered):
  if len(CatchDiscards)!=0:
   count2=0;
   while count2<len(CatchDiscards):
+   print("count2 ",count2)
+   print("CatchDiscards[count2] ",CatchDiscards[count2])
+   print("FilteredFrame[count2] ", FilteredFrame[count2])
    #print("Entered the second while loop count2 in Filter= ",count2)
    #print(CatchDiscards[count2])
    #FilteredFrame=FilteredFrame.drop(CatchDiscards[count2])
