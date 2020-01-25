36619f75375c1f5b16abb26f0c89dfa17ecb722d
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-25 06:02:27 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -87,10 +87,9 @@ def filterNonParticipators(FrameToBeFiltered):
   #print("Brand ",BrandFilter)
   if FilterString.find(BrandFilter)!=-1:
    CatchDiscards.append(count)
-   if BrandFilter!="Luv Homes":#!="Clayton Homes" or BrandFilter !="Craftmark Homes" or BrandFilter !="Luv Homes" :
+   if BrandFilter!="Clayton Homes" or BrandFilter!="Craftmark Homes" or BrandFilter!="Luv Homes":
     print("Not Luv Homes ",BrandFilter)
-   if BrandFilter=="Luv Homes": 
-    print("Nomrlly should be Luv Homes ",BrandFilter)
+  
    #print("Brand ",BrandFilter," found at row ",count," position ",FilterString.find(BrandFilter)," is being Dropped")
   if FilterString.find(CommIDFilter)!=-1:
    CatchDiscards.append(count)
