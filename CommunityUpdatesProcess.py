192a06ae28bdd6249257a0bb74133467c4f0545b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-25 12:14:47 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -123,7 +123,7 @@ def filterNonParticipators(FrameToBeFiltered):
    #print(FilteredFrame)
    #FilteredFrame=FilteredFrame.drop([6])
    #FilteredFrame=FilteredFrame.drop([96])
-   FilteredFrame2=FilteredFrame.drop([CatchDiscards[count2]])
+   FilteredFrame=FilteredFrame.drop(CatchDiscards[count2])
    count2+=1; 
   print("Length After ",len(FilteredFrame))   
  print("End Filter") 
