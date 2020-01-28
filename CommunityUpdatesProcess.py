b8cf309b1e639a5d9d509bc8a670f75bc15631d1
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-28 12:05:49 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -170,17 +170,6 @@ def filterNonParticipators(theFrame):
  DeDupstring=""
  icount=5;
  while icount<len(theFrame['Community Name']):
-  """
-  Community=str(theFrame["Community Name"][icount]).replace("40s","").replace("40'","").replace("40","").replace("45s","")\
-  .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
-  .replace("55'","").replace("55","").replace("60s","").replace("60'","").replace("60","").replace("65s","")\
-  .replace("65'","").replace("65","").replace("70s","").replace("70'","").replace("70","").replace("75s","")\
-  .replace("75'","").replace("75","").replace("80s","").replace("80'","").replace("80","").replace("85s","")\
-  .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
-  .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
-  .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
-  """
-  
   try:
    Community=str(theFrame["Community Name"][icount]).replace("40s","").replace("40'","").replace("40","").replace("45s","")\
    .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
@@ -193,40 +182,34 @@ def filterNonParticipators(theFrame):
    
    Community=theFrame["Community Name"][icount]
    DeDupstring=DeDupstring+" "+Community
-   print(Community)
-   print("Successfull fire ",icount)
+   #print(Community)
+   #print("Successfull fire ",icount)
   except:
    print("Row Skipped ",icount);
-   #.replace("40s","")
-  """
-  .replace("40'","").replace("40","").replace("45s","")\
-  .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
-  .replace("55'","").replace("55","").replace("60s","").replace("60'","").replace("60","").replace("65s","")\
-  .replace("65'","").replace("65","").replace("70s","").replace("70'","").replace("70","").replace("75s","")\
-  .replace("75'","").replace("75","").replace("80s","").replace("80'","").replace("80","").replace("85s","")\
-  .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
-  .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
-  .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
-  """
-  #DeDupstring=DeDupstring+" "+Community
-  print(Community)
+  DeDupstring=DeDupstring+" "+Community
   icount+=1;
- print(DeDupstring) 
   
+ icount0=0;
+ while icount0<len(theFrame['Community Name']):
+  try:
+   Community=str(theFrame["Community Name"][icount]).replace("40s","").replace("40'","").replace("40","").replace("45s","")\
+   .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
+   .replace("55'","").replace("55","").replace("60s","").replace("60'","").replace("60","").replace("65s","")\
+   .replace("65'","").replace("65","").replace("70s","").replace("70'","").replace("70","").replace("75s","")\
+   .replace("75'","").replace("75","").replace("80s","").replace("80'","").replace("80","").replace("85s","")\
+   .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
+   .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
+   .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
+   
+   Community=theFrame["Community Name"][icount];
+   if DeDupstring.count(Community)>0:
+    print("found in string ",DeDupstring.count(Community)," times");
+   
+  except:
+   print("Exception on Second Loop ")
  
- """
- theFrame=theFrame['Community Name'].replace("40s","").replace("40'","").replace("40","").replace("45s","")\
- .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
- .replace("55'","").replace("55","").replace("60s","").replace("60'","").replace("60","").replace("65s","")\
- .replace("65'","").replace("65","").replace("70s","").replace("70'","").replace("70","").replace("75s","")\
- .replace("75'","").replace("75","").replace("80s","").replace("80'","").replace("80","").replace("85s","")\
- .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
- .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
- .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
- """
- #theFrame=theFrame.drop_duplicates();
- #print("Length theFrame=theFrame.drop_duplicates() ",len(theFrame))
- print("theFrame ",theFrame)
+ 
+ 
                                                             
  
  return theFrame;
