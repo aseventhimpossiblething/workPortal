e7e38dd1fa7dc09726f4fd3f456bdc56064d97cb
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-29 01:46:10 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -173,15 +173,9 @@ def filterNonParticipators(theFrame):
    .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
    .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
    .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
-   #print("Community string and replace")
+  
    Community=theFrame["Community Name"][icount]
-   #DeDupArray.append(Community)
    
-   #print("Community=theFrame['Community Name'][icount] ")
-   #DeDupstring=DeDupstring+" "+Community
-   #print("DeDupSting Grew")
-   #print(Community)
-   #print("Successfull fire ",icount)
   except:
    Community="  !!!  "
    #print("Row Skipped ",icount);
@@ -202,10 +196,13 @@ def filterNonParticipators(theFrame):
    .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\
    .replace("95'","").replace("95","").replace("100s","").replace("100'","").replace("100","").replace("105s","")\
    .replace("105'","").replace("105","").replace("110s","").replace("110'","").replace("110","")
-   
    Community=theFrame["Community Name"][icount0];
    
    #Community.drop()
+   print("DeDupArray length in second loop before if",len(DeDupArray));
+   print("DeDupArray count of community in second loop before if ",DeDupArray.count(Community));
+   print("Community before if ",Cummunity);
+   
    if DeDupArray.count(Community)>0:
     print("found in string ",DeDupstring.count(Community)," times");
     theFrame.drop([icount0])
