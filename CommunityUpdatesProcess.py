48b89e042fdb932b7daeb3e914e08bf0353ce599
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-01-29 07:08:17 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -216,8 +216,8 @@ def filterNonParticipators(theFrame):
   except:
    print("Second Loop failed Count ",icount0)
    icount0+0;
-  if icount%100==0:
-  print("Second Loop Count ",icount0)
+  if icount0%100==0:
+   print("Second Loop Count ",icount0)
   icount0+=1; 
  
  print("End of Filter ")
