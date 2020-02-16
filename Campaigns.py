818f8a3e969f132573d54fb09070978857c451a9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-02-15 21:21:35 -0600

    Update Campaigns.py

--- Campaigns.py
+++ Campaigns.py
@@ -7,45 +7,42 @@ client = GoogleAdsClient.load_from_storage()
 
 
 #def CampFunc():
- print("Begin CampFunc()")
- PAGE_SIZE = 100
- def main(client):
-  print("Begin to define main(client)()")
-  # Initialize appropriate service.
-  print("client.GetService('CampaignService', version='v201809')")
-  campaign_service = client.GetService('CampaignService', version='v201809')
-  # Construct selector and get all campaigns.
-  offset = 0
-  selector = {
-      'fields': ['Id', 'Name', 'Status'],
-      'paging': {
-          'startIndex': str(offset),
-          'numberResults': str(PAGE_SIZE)
-      }
-   }
-
+print("Begin CampFunc()")
+PAGE_SIZE = 100
+def main(client):
+ print("Begin to define main(client)()")
+ # Initialize appropriate service.
+ print("client.GetService('CampaignService', version='v201809')")
+ campaign_service = client.GetService('CampaignService', version='v201809')
+ # Construct selector and get all campaigns.
+ offset = 0
+ selector = {
+     'fields': ['Id', 'Name', 'Status'],
+     'paging': {
+         'startIndex': str(offset),
+         'numberResults': str(PAGE_SIZE)
+     }
+  }
   more_pages = True
-  print("Begin while for pages")
-  while more_pages:
-    page = campaign_service.get(selector)
-
+ print("Begin while for pages")
+ while more_pages:
+   page = campaign_service.get(selector)
     # Display results.
-    if 'entries' in page:
-      for campaign in page['entries']:
-        print('Campaign with id "%s", name "%s", and status "%s" was '
-              'found.' % (campaign['id'], campaign['name'],
-                          campaign['status']))
-    else:
-      print('No campaigns were found.')
-    offset += PAGE_SIZE
-    selector['paging']['startIndex'] = str(offset)
-    more_pages = offset < int(page['totalNumEntries'])
+   if 'entries' in page:
+     for campaign in page['entries']:
+       print('Campaign with id "%s", name "%s", and status "%s" was '
+             'found.' % (campaign['id'], campaign['name'],
+                         campaign['status']))
+   else:
+     print('No campaigns were found.')
+   offset += PAGE_SIZE
+   selector['paging']['startIndex'] = str(offset)
+   more_pages = offset < int(page['totalNumEntries'])
 
-
-  if __name__ == '__main__':
-   adwords_client = adwords.AdWordsClient.LoadFromStorage()
-   main(adwords_client)
-   cat="2"
-   
+ if __name__ == '__main__':
+  adwords_client = adwords.AdWordsClient.LoadFromStorage()
+  main(adwords_client)
+  cat="2"
+  
    return "cat"
   
