3e00c9bd91cf4f6d4d59cd2f3bd72cfef9249dbf
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-02-16 05:28:07 -0600

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -29,7 +29,7 @@ from googleads import adwords
 #client = GoogleAdsClient.load_from_storage()
 #from googleads import adwords
 client2 = GoogleAdsClient.load_from_storage()
-client.GetService()
+client2.GetService()
 adwords_client1 = adwords.AdWordsClient.LoadFromStorage()
 adwords_client2 = AdWordsClient.LoadFromStorage()
 
