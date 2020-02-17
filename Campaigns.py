6fba846d73f562008869194b92c4afd5225e04a9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-02-17 00:36:00 -0600

    Update Campaigns.py

--- Campaigns.py
+++ Campaigns.py
@@ -2,6 +2,8 @@
 from google.ads.google_ads.client import GoogleAdsClient
 client = GoogleAdsClient.load_from_storage()
 #print(dir(client))
+#below is new
+import google.ads.google_ads.client
 ServiceScan=client.get_service('GoogleAdsService', version='v2')
 print(dir(ServiceScan))
 client2=google.ads.google_ads.client.GoogleAdsClient.load_from_storage()
