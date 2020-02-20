22c282571a2fcd1f6770bbf6855418278c8c653c
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-02-20 10:58:23 -0600

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -117,11 +117,16 @@ def CommListFileHandler():
     print("ImmutableMultiDict([('Communities', = ",str(simplereq).find("ImmutableMultiDict([('Communities',"))
     if  str(simplereq).find("ImmutableMultiDict([('Communities',")!=-1:
         print("This is from the Community Sheet")
-    else: 
-   print("simplereq.files")
-   print(simplereq['Communities'])     
-   print(simplereq['Communities'])
-   print(simplereq['Communities'].filename)
+    else:
+    print("simplereq.files['communities']")
+    print(simplereq.files['communities'])     
+    print("simplereq.files")
+    print(simplereq.files)
+    print("simplereq['Communities']")     
+    #print(simplereq['Communities'])     
+    print(simplereq['Communities'])
+    print("simplereq['Communities'].filename")
+    print(simplereq['Communities'].filename)
     
        
     """
