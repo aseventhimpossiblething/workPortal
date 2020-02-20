72c058105a6c1e993898b6148095d5ffc9f7befe
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-02-20 11:43:06 -0600

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -110,9 +110,10 @@ def CommListFileHandler():
     else:
         print(" asks is arandomthing is present seems to be no ");
     """    
-    print("request ",request)     
+    print("request ",request)  
+    print(dir(request))    
     simplereq=request.files
-    print("Simple request accepted ")
+    print("Simplereq= request.files ")
     print("simplereq is =  ",simplereq)
     print("ImmutableMultiDict([('Communities', = ",str(simplereq).find("ImmutableMultiDict([('Communities',"))
     if  str(simplereq).find("ImmutableMultiDict([('Communities',")!=-1:
