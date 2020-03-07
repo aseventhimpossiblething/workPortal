56faf60150bd26f93c3c506422a62cd14f7f27fa
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-07 03:21:56 -0600

    Update reportexpFreeToDelete.py

--- reportexpFreeToDelete.py
+++ reportexpFreeToDelete.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from googleads import adwords
 #from google.ads.google_ads.client import GoogleAdsClient
@@ -6,7 +7,10 @@ from googleads import adwords
 client=adwords.AdWordsClient.LoadFromStorage()
 
 print("Experiment module Loaded")
-
+os.chdir("/app/.heroku/python/lib/python3.6/site-packages/googleads")
+trace=open("common.py","r+")
+print(trace.read())
+print(/app/.heroku/python/lib/python3.6/site-packages/googleads/common.py)
 
 #campaign_service = client.get_service('CampaignService', version='v2')
 campaign_service = client.GetService('CampaignService', version='v201809')
