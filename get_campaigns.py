3ef34a13f785192016733e49efbe83499fcf20af
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-11 16:32:22 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -26,6 +26,7 @@ from google.ads.google_ads.errors import GoogleAdsException
 
 
 def main(client, customer_id):
+    print("is main eve running")
     ga_service = client.get_service('GoogleAdsService', version='v3')
 
     query = ('SELECT campaign.id, campaign.name FROM campaign '
