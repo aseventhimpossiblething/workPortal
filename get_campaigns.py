b1d63220bf88d0828315d3ea4f53fca950258907
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-11 17:13:50 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -27,7 +27,7 @@ from google.ads.google_ads.errors import GoogleAdsException
 
 def main(client, customer_id):
     print("is main even running")
-    ga_service = client.get_service('GoogleAdsService', version='v3')
+    ga_service = client.get_service('GoogleAdsService', version='v2')#orginal instructions said v3 but it fails at runtime with error there is no va3
 
     query = ('SELECT campaign.id, campaign.name FROM campaign '
              'ORDER BY campaign.id')
