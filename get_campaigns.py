b1d1bf18f168e31d122a81569eb3a22de76ae069
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-13 19:11:28 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -96,6 +96,7 @@ print('_has_next_page ',theDataOBj._has_next_page)
 print(CustomerService)
 print(dir(CustomerService))
 print(CustomerService.get_customer)
+print(dir(CustomerService.get_customer))
 #theDataOBj[1]
 #print(ga_service.search("9662896891",query))
 #print(dir(ga_service.search("9662896891",query)))
