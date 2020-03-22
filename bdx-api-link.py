64fef5a8fcb63895d33377af384f61dd231da69b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-22 15:23:20 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -22,7 +22,7 @@ from flask_sslify import SSLify
 app = Flask(__name__,"/static/")
 #sslify = SSLify(app)
 print("Loaded into Gunicorn and Log runs Correctly")
-print("current directory - ",os.getcwd)
+print("current directory - ",os.getcwd())
 
 #DATABASE_URL = os.environ['DATABASE_URL']
 
