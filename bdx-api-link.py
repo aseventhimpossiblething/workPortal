d654a0724288d5bcdab27f7129a714971b7e95ef
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-23 10:29:19 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -109,7 +109,7 @@ def testtextfile():
 
 @app.route('/DisplayCommUpdate')
 def CommUpdateDisplay():
-    os.chdir('/app/Sheets/')     
+    os.chdir('var/www/workPortal/Sheets/')     
     storeRequest=open('RequestsVsResponses.txt','r+')
     read_storeRequest=storeRequest.read()
     storeRequest.close()
