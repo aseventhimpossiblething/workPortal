513d0db7ca455c26d3d7fac6757f669919c9e8a5
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-26 19:14:27 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -52,7 +52,7 @@ def BidOpFileHandler():
     if isTrainingSheet!=-1:
        os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
        #Temp=Pandas.DataFrame(Temp,columns='Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top','Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type')
-       Temp=Pandas.DataFrame(Temp, columns=Campaign)
+       Temp=pandas.DataFrame(Temp, columns='Campaign')
        print(Temp)
        pandas.DataFrame('BidOpSeed.xlsx')
        #writer=pandas.ExcelWriter('DefaultSheet.xlsx')
