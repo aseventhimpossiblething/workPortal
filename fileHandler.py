43f555b0b8cb2bb1190b30a78c0982e18beee348
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-26 19:35:03 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -53,8 +53,9 @@ def BidOpFileHandler():
        os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
        Temp=pandas.DataFrame(Temp,columns=['Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top','Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type'])
        #Temp=pandas.DataFrame(Temp, columns='Campaign')
-       print(Temp)
-       pandas.DataFrame('BidOpSeed.xlsx')
+       #print(Temp)
+       CoreTrainingData=pandas.DataFrame.read_excel('BidOpSeed.xlsx')
+       print(CoreTrainingData)
        #writer=pandas.ExcelWriter('DefaultSheet.xlsx')
        #.to_excel(writer)
        isTrainingSheet='This is Training Sheet will be added to the body of training Data'
