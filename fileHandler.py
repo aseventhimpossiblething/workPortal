89f46f7fe4d57337b5af9aa89e630bbdc9f2de2b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-27 10:29:32 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -39,19 +39,21 @@ def BidOpFileHandler():
     isTrainingSheet=str(Temp.columns).find('New Bid') 
     if isTrainingSheet!=-1:
        os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
-       Temp=pandas.DataFrame(Temp,columns=['Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top','Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type'])
+       Temp=pandas.DataFrame(Temp,columns=['Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type'])
        CoreTrainingData=pandas.read_excel('BidOpSeed.xlsx')
+       print("----------------------------------Temp------------------------")
        print(Temp)
-       """
+       print('--------------------------Core Training Data------------------')
        print(CoreTrainingData)
        print("_________________________________________________________________________________________") 
        print("_________________________________________________________________________________________") 
        #CoreTrainingData=CoreTrainingData.append(Temp, sort='False')
        print("_________________________________________________________________________________________") 
        print("_________________________________________________________________________________________") 
+       print("--------------------------Merged New Core Data-------------------------") 
        print(CoreTrainingData)
        #writer=pandas.ExcelWriter('DefaultSheet.xlsx')
-       """
+       
        #.to_excel(writer)
        isTrainingSheet='This Training Sheet will be added to the body of training Data'
     else:
