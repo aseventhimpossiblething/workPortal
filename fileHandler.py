9dcc5c138b1a38b1121027f25f9a986163797473
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-27 17:01:34 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -58,8 +58,8 @@ def BidOpFileHandler():
        #CoreTrainingData.to_excel('BidOpSeed.xlsx')
        CoreTrainingData.to_excel('BidOpSeedViewable.xlsx')
        print(CoreTrainingData)
-       print("[]",CoreTrainingData[0])
-       print("[[]]",CoreTrainingData[[0]])
+       print("['Match type']",CoreTrainingData['Match type'])
+       print("[['Match type']]",CoreTrainingData[['Match type']])
        CoreTrainingData=pandas.DataFrame(CoreTrainingData,columns=['Changes','Campaign','Ad group','Match type','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget']) 
        
        CoreTrainingData.to_excel('BidOpSeed.xlsx')
