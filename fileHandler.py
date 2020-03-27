3fada26c106f47f967b463f8bb989ec3ce257442
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-26 19:28:34 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -51,7 +51,7 @@ def BidOpFileHandler():
     isTrainingSheet=str(Temp.columns).find('New Bid') 
     if isTrainingSheet!=-1:
        os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
-       Temp=Pandas.DataFrame(Temp,columns=['Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top','Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type'])
+       Temp=pandas.DataFrame(Temp,columns=['Campaign','Ad group','Keyword','Changes','New Bid','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top','Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget','Match type'])
        #Temp=pandas.DataFrame(Temp, columns='Campaign')
        print(Temp)
        pandas.DataFrame('BidOpSeed.xlsx')
