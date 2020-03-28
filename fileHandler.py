469faa08fe63bb5ce05e263d5f3464e658aa4f0f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-27 21:12:20 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -64,7 +64,7 @@ def BidOpFileHandler():
                CoreTrainingData['Match type']=Match_Type;
         
            #print("CoreTrainingData['Match type']",CoreTrainingData['Match type'])         
-           CoreTrainingData=pandas.DataFrame(CoreTrainingData,columns=['Changes','Campaign','Ad group','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget']) 
+           CoreTrainingData=pandas.DataFrame(CoreTrainingData,columns=['Changes','Campaign','Ad group','Match Type','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank','IS lost to budget']) 
        
            AdGroup=[];
            for kw in CoreTrainingData['Ad Group']:
