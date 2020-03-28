a89f743f0b03cba89a0f7566328c7d74e7b7ef77
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-28 10:01:32 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -76,7 +76,7 @@ def BidOpFileHandler():
                print("2-",kw) 
                kw=str(re.search('>\d+',kw))
                print("3- target str ",kw)
-               targLoc=kw.find('match='>1')
+               targLoc=kw.find("match='>")
                #match='>1
                print(kw) 
                Adgroup.append(kw)
