f43c37e2552126368e6088a2c25c9c2f5d60d19a
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-28 11:44:33 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -75,7 +75,7 @@ def BidOpFileHandler():
            count=0;      
            Adgroup=[];
            for kw in Temp['Ad group']:
-               if str(re.search('>\d+',kw)=-1:
+               if re.search('>\d+',kw)=-1:
                       kw="match='>0"
                kw=str(re.search('>\d+',kw))
                targLoc=kw.find("match='>")
