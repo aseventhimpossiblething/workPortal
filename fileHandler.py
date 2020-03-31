c7ed4f35166e33194954ce0a3ff486f84b55462f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-03-31 18:07:11 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -85,6 +85,7 @@ def BidOpFileHandler():
                 print(kw)
                 print(type(kw))
                 print(int(kw))
+                print(type(kw)
                 kw=(int(kw))(1000);
                #print("Timeout on second pass count ",ccountr)
                print(kw)
