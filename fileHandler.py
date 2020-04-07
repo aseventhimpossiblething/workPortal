c7a0172297b94e05a3e1840e604ed0b4aa232929
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-07 08:39:40 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -167,7 +167,7 @@ def BidOpFileHandler():
            
            return "<html><a href='/BasisOfBids'>This Training Sheet will be added to the body of training Data Click to view Basis Sheet</a></html>"
        #TrainBehavior(Temp);
-       TrainLoad=threading.Thread(target=TrainBehavior args="x");
+       TrainLoad=threading.Thread(target=TrainBehavior, args="x");
        TrainLoad.start(); 
        #return "Sheet has Been Identified as Training Data it is being formatted and Loaded as such... please wait.. do not press back button"  
        print("is there an attempt to return")
