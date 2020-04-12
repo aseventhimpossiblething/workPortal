2500630dedf6e4de549a116c0c4a4078934cb705
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-12 06:14:40 -0500

    Update appconfig.py

--- appconfig.py
+++ appconfig.py
@@ -1,2 +1,6 @@
---access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log
+accesslog="/var/log/gunicorn/access.log"
+errorlog="/var/log/gunicorn/error.log"
+
+
+#--access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log
 bind="127.0.0.1:5000"
