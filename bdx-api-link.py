b6ce000b22b27740ff7f17d823a0f7240bda3b2a
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-12 07:51:57 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -1,7 +1,7 @@
 domain="http://bhiapilink.com"
 import tensorflow
 import CommunityUpdatesProcess
-#import GetCampaigns
+import GetCampaigns
 import glob
 #import numpy
 #import scipy
@@ -12,7 +12,7 @@ import os
 from flask import Flask, Markup, render_template, request
 from flask import send_file
 from flask import send_from_directory
-import get_campaigns
+#import get_campaigns
 #import expermientCampLabels
 #import Campaigns
 #import reportexpFreeToDelete
