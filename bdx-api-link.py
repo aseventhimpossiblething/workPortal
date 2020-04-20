3efb4c9d7735a86465f9d4901bc1c20766c9068b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-20 16:42:17 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -14,9 +14,9 @@ from flask import Flask, Markup, render_template, request
 from flask import send_file
 from flask import send_from_directory
 print("Before get_campaigns")
-import get_campaigns
+#import get_campaigns
 print("Before expermientCampLabels")
-import expermientCampLabels
+#import expermientCampLabels
 print("After expermientCampLabels")
 #import Campaigns
 print("After Campaigns")
