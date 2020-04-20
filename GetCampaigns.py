8ec195f43a6ec537eacc8ef4e835e78c4c16e15b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-20 17:38:28 -0500

    Update GetCampaigns.py

--- GetCampaigns.py
+++ GetCampaigns.py
@@ -67,4 +67,4 @@ if __name__ == '__main__':
   adwords_client = adwords.AdWordsClient.LoadFromStorage()
   main(adwords_client)
   
-adwords_client = adwords.AdWordsClient.LoadFromStorage()
+#adwords_client = adwords.AdWordsClient.LoadFromStorage()
