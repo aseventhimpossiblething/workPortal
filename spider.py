d0412b1e743b133075a5433b028873eab01e71d8
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-27 14:37:02 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 import os
 os.system("echo 'bash'")
-spiderCmmnd="wget -r -nd -nv https://www.newhomesource.com --spider -o spiderfile"
+spiderCmmnd="wget -r -nd https://www.newhomesource.com --spider -o spiderfile"
 retrieveNmbr="echo spiderfile|grep ' 200' "
 os.system(spiderCmmnd)
 os.system(retrieveNmbr)
