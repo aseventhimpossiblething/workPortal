177f4914ae3d87b9e649bfc641b5a8089a4c61f9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-27 14:45:50 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,6 +1,10 @@
 import os
-os.system("echo 'bash'")
-spiderCmmnd="wget -r -nd https://www.newhomesource.com --spider -o spiderfile"
-retrieveNmbr="echo spiderfile|grep ' 200' "
-os.system(spiderCmmnd)
-os.system(retrieveNmbr)
+import threading
+def spider
+    os.system("echo 'bash'")
+    spiderCmmnd="wget -r -nd https://www.newhomesource.com --spider -o spiderfile"
+    retrieveNmbr="echo spiderfile|grep ' 200' "
+    os.system(spiderCmmnd)
+    os.system(retrieveNmbr)
+launchSpider=threading.Thread(spider)
+launchSpider.start()
