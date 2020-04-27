dc4a8023554c2a8858cbfd6077fe00d07c56e020
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-27 18:58:01 -0500

    Update spiderGrep.sh

--- spiderGrep.sh
+++ spiderGrep.sh
@@ -3,10 +3,4 @@ touch brokenLines
 echo running grep
 cat spiderfile | grep -n " broken" >brokenLines
 cat brokenLines
-sleep 20s
-echo waiting
-#sleep 10
-cat spiderfile | grep -n " broken" >brokenLines
-cat brokenLines
-#sleep 20
 echo finished grep
