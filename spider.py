fcf8b0816ee226c0e95d03bd41f430c3a9cba28e
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-27 16:46:51 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@ def spider():
     spiderCmmnd="wget -r -b -nd https://www.newhomesource.com --spider -o spiderfile"
     retrieveNmbr="cat spiderfile"
     os.system(spiderCmmnd)
+    print("_____________")
     os.system(retrieveNmbr)
     os.system("echo 'bash2'")
 #launchSpider=threading.Thread(target=spider)
