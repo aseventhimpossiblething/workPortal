37d184efd0b286ade5790168af264c7259708946
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-28 07:53:32 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -4,31 +4,18 @@ import time
 target_Site="https://www.newhomesource.com"
 def spider(targetSite):
     os.system("echo 'bash'")
-    #targetSite="https://bdxapilink.com"
-    #targetSite="https://www.newhomesource.com"
     targetSite=targetSite
     spiderCmmnd="wget -r -b -nd "+targetSite+" --spider -o spiderfile"
-    #spiderCmmnd="wget -r -b -nd https://www.newhomesource.com --spider -o spiderfile"
     retrieveNmbr="bash spiderGrep.sh"
     os.system(spiderCmmnd)
-    #print("30 sec pause")
-    #time.sleep(10)
-    #print("_____________")
-    #os.system(retrieveNmbr)
-    #time.sleep(10)
-    #os.system(retrieveNmbr)    
-    #os.system("touch brokenLines")
-    #os.system("cat spiderfile | grep -a ' broken' >brokenLines")
+    
     os.system("bash midnighthowl.sh")
-    #time.sleep(10)
-    #os.system("bash midnighthowl.sh")
     
     spiderfile=open('spiderfile','r')
     spiderlines=spiderfile.readlines()
-    print(spiderlines[2])
     spiderfile.close()
     
-    print("=",spiderlines)
+    #print("=",spiderlines)
     """
     time.sleep(20)
     spiderfile=open('spiderfile','r')
