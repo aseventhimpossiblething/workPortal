f3d4887e7385ea664cdbc2587548b57fced1992f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-04-27 19:05:15 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,13 +1,17 @@
 import os
 import threading
+import time
 def spider():
     os.system("echo 'bash'")
     spiderCmmnd="wget -r -b -nd https://www.newhomesource.com --spider -o spiderfile"
     retrieveNmbr="bash spiderGrep.sh"
-    #os.system("touch brokenLines")
     os.system(spiderCmmnd)
+    print("10 sec pause")
+    time.sleep(10)
     print("_____________")
     os.system(retrieveNmbr)
+    time.sleep(10)
+    os.system(retrieveNmbr)    
     #os.system("touch brokenLines")
     os.system("echo 'bash2'")
 launchSpider=threading.Thread(target=spider)
