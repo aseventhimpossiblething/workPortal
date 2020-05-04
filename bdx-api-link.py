69529ecc1564b1a2e20e8d57929c59781fb4a60b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-04 02:24:39 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -84,7 +84,7 @@ def chckbdxcred():
     x=request.cookies.get(setCnam());
     print("get cookie = ",x," bdxcred = ",bdxcred());
     print("x==bdxcred() = ",x==bdxcred())
-    if x==bdxcred():
+    if (x==bdxcred())=='True':
        a="<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL="
        b=login_page
        c="'><html>did not forward</html>"
