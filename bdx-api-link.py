c886c2c5951293c443511b3d44adcdefffc361cb
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-03 23:52:42 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -102,7 +102,7 @@ def mlgn():
 
 @app.route(login_page2)
 def mlgne():
-    gencook=make_response("form2");
+    gencook=make_response("<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL=/'><html>did not forward</html>");
     gencook.set_cookie(setCnam(),bdxcred());
     return gencook
 
