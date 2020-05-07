90d3a584af05b966b0bac356a32eda94260ae557
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-07 16:00:28 -0500

    Update appconfig.py

--- appconfig.py
+++ appconfig.py
@@ -1,5 +1,8 @@
+NUM_WOKERS=6
+TIMEOUT=120
 accesslog="/var/log/gunicorn/access.log"
 errorlog="/var/log/gunicorn/error.log"
+
 
 
 #--access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log
