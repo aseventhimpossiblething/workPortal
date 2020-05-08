0d0807572e1c56c6e40af30b022ae9373562e60e
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-08 10:11:07 -0500

    Update spider.py

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@ import time
 target_Site="https://www.newhomesource.com"
 def sendSpider(targetSite):
     os.system("echo 'bash'")
+    os.system("touch spiderfile")
     targetSite=targetSite
     spiderCmmnd="wget -r -b -nd "+targetSite+" --spider -o spiderfile"
     retrieveNmbr="bash spiderGrep.sh"
