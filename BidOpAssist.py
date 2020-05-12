25d059327f96146c8d29b3e6e0a4283a6501128a
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-12 18:23:09 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -233,7 +233,7 @@ def BidOpOverview(desiCols,corecols,change,Temp):
     #print("Temp ",Temp[['Keyword']])
     
     #print(TempForOutPut)
-    @print(TempForOutPut[[TempForOutPut.columns.values[0],TempForOutPut.columns.values[1],TempForOutPut.columns.values[2],TempForOutPut.columns.values[3],TempForOutPut.columns.values[4],TempForOutPut.columns.values[5],TempForOutPut.columns.values[6],TempForOutPut.columns.values[7],TempForOutPut.columns.values[8],TempForOutPut.columns.values[9],TempForOutPut.columns.values[10]]])
+    #print(TempForOutPut[[TempForOutPut.columns.values[0],TempForOutPut.columns.values[1],TempForOutPut.columns.values[2],TempForOutPut.columns.values[3],TempForOutPut.columns.values[4],TempForOutPut.columns.values[5],TempForOutPut.columns.values[6],TempForOutPut.columns.values[7],TempForOutPut.columns.values[8],TempForOutPut.columns.values[9],TempForOutPut.columns.values[10]]])
     
     
     OutputBid=Model.predict(TempForOutPut); 
