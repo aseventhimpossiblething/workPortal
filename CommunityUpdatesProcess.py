cc7afbc98008027c84b110c752ad40dee4e7f1ac
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-21 12:15:22 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -466,7 +466,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    Final_URL.append(URL_Struct1)
  
   
-   Keyword_conv=Keyword_conv.replace("+++","+").replace("+ + +","+").replace(" + + + ","+");.replace(" + + +","+")\
+   Keyword_conv=Keyword_conv.replace("+++","+").replace("+ + +","+").replace(" + + + ","+").replace(" + + +","+")\
                   .replace("+ + + ","+").replace("++","+").replace("+ +","+").replace(" ++","+").replace("++ ","+")\
                   .replace(" + +","+").replace("+ + ","+");
    if len(Keyword_conv)<12:
