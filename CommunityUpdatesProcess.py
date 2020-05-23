d94b36687636b0f678a790751a867bca8fc09cd3
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-22 21:40:48 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -479,13 +479,15 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    
    if str(NewDataFrame['City'][count][:5]).find(str(NewDataFrame['Community Name'][count]))>-1:
      print("City found in community","City = ",NewDataFrame['City'][count],"::: Community = ",NewDataFrame['Community Name'][count]);
+     print("----------------------------------------------------------------------------------------------------------------------------")
    if str(NewDataFrame['City'][count]).find(str(NewDataFrame['Community Name'][count]))>-1 or str(NewDataFrame['Community Name'][count]).find(str(NewDataFrame['City'][count]))>-1:
-      print("City = ",NewDataFrame['City'][count],"---- Community Name = ",NewDataFrame['Community Name'][count]); 
+     print("City = ",NewDataFrame['City'][count],"---- Community Name = ",NewDataFrame['Community Name'][count]); 
+     print("----------------------------------------------------------------------------------------------------------------------------")
    else:
     
     print("Not marked......City = ",NewDataFrame['City'][count][:5],":::: Community Name = ",NewDataFrame['Community Name'][count])
     print(str(NewDataFrame['City'][count][:5]).find(str(NewDataFrame['Community Name'][count])))
-    print("---------------------------------------------------------------------------------------------------------")
+    print("-------------------------")
   except:
    NewDataFrame=NewDataFrame.drop([count])
   count+=1;
