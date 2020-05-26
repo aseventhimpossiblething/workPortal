59b4c7318159c721b1b8009dd71ba7573c5536d5
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-26 10:04:07 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -480,6 +480,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    label="Created by WebApp"  
    #Label.append(label);
    
+   cityprime=str(NewDataFrame['City'][count]).lower().replace("-"," ").replace("_"," ").replace(","," ");
    city=str(NewDataFrame['City'][count]).lower().replace("-"," ").replace("_"," ").replace(","," ");
    wordBoundry=(city.find(" "))+1;
    city=city[:wordBoundry];
@@ -497,7 +498,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    #if str(NewDataFrame['City'][count][:5]).lower().find(str(NewDataFrame['Community Name'][count]))>-1:
      print("City is in Community")
      #print("City found in community","City = ",NewDataFrame['City'][count],"::: Community = ",NewDataFrame['Community Name'][count]);
-     print("City found in community","City = ",city,"::: Community Name = ",community);
+     print("cityprime-",cityprime,"  :::City found in community","City = ",city,"::: Community Name = ",community);
      label=label+";City Name as Part of Community Name"
      print("----------------------------------------------------------------------------------------------------------------------------")
    #if str(NewDataFrame['City'][count]).find(str(NewDataFrame['Community Name'][count]))>-1 or str(NewDataFrame['Community Name'][count]).find(str(NewDataFrame['City'][count]))>-1:
