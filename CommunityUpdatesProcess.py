a69b2963be5a73755f9c98ee199bc9a03d4f8c77
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-05-26 16:48:25 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -488,7 +488,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
       print(int(lenCommunity))
       print(int(lenCity))
       
-      if len(community)==len(community):
+      if len(community)==len(city):
          print("Both equal");
          #print("community in ==case  ",community);
          #print("city ==case  ",city);
