f4f97236ecd6418ff9c39f69ad924716fb90ca33
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-02 09:33:11 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -146,7 +146,7 @@ for subset in response:
       print("NovDump[1] = ",NovDump[1]);
       
       json.loads(NovDump);
-      print(json.loads(NovDump)[0]);
+      print(json.loads(NovDump)[result]);
       #print("str(subset)['result'] ",str(subset)['result']);      
       #subsetStr=type(subset);
       #print("type(subsetStr) ",type(subsetStr));
