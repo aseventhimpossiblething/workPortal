53f861e0a28c4fa0180f4bb42d0f1e22487f36ef
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-02 10:57:20 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -137,8 +137,8 @@ for subset in response:
       #print('str(subset) ',str(subset));
       #print("dir(subset) ",dir(subset))
       #print('str(subset)[0] ',str(subset)[0]);
-      #json.loads(subset['results']['campaign'])
-      json.dumps(subset['results']['campaign'])
+      json.loads(subset['results']['campaign']['resource_name'])
+      json.dumps(subset['results']['campaign']['resource_name'])
       
       #str(subset).json.load();
       #str(subset).json.loads();
