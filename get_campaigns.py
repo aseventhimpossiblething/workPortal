e08de152554a4c2815444eff805d2aa9c7559149
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-02 09:08:45 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -140,7 +140,8 @@ for subset in response:
       
       #json.loads(str(subset));
       NovDump=json.dumps(str(subset));
-      print("NovDump = ",NovDump);
+      print("NovDump[results] = ",NovDump['results']);
+       print("NovDump[campaign] = ",NovDump['campaign']);
       
       
       #print("str(subset)['result'] ",str(subset)['result']);      
