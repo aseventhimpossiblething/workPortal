be309619ecf9b0ed80d31a9edea68082520fd9c0
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-04 16:18:48 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -41,10 +41,11 @@ for subset in response:
 def fromAds(customer_id,query):      
     #google_ads_client = GoogleAdsClient.load_from_storage('google-ads.yaml')
     #ga_service = google_ads_client.get_service('GoogleAdsService', version='v3')
+    response = ga_service.search_stream(customer_id, query=query)  
     for subset in response:  
         jsonObj=json_format.MessageToJson(subset)
         jsonObj=json.loads(jsonObj)  
-        response = ga_service.search_stream(customer_id, query=query)
+        #response = ga_service.search_stream(customer_id, query=query)
         countOfSubset=0; 
         while len(jsonObj)>countOfSubset:
             print("jsonObj['results'][",countOfSubset,"]['campaign']['name'] ",jsonObj["results"][countOfSubset]["campaign"]["name"])
