3e06e980cc64519d2f869a8990fb33ecfa046012
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-04 09:26:11 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -133,8 +133,9 @@ for subset in response:
       print('subset ',subset);
       
       
+      json_format.MessageToJson(subset)
       #row.recommendation.text_ad_recommendation.ad  
-      subset.recommendation.text_ad_recommendation.ad  
+      #subset.recommendation.text_ad_recommendation.ad  
       #print("subset[0] ",subset[0]);
       #print("subset[1] ",subset[1]);
       #print("subset[2] ",subset[2]);
