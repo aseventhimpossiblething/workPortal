b418282f4635299c0ba894f00a1d3579432ef5cf
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-06 11:50:23 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -42,20 +42,14 @@ query = ('SELECT campaign.id, campaign.name, campaign.status, campaign_budget.am
 
 
 def fromAds(customer_id,query):
+    newTable=[campaignName,campaignCost,campaignClicks,campaignConversions,campaignImpressions,campaignBudget,campaignStatus];
     print(customer_id) 
     customer_id=customer_id.replace("-","") 
-    #google_ads_client = GoogleAdsClient.load_from_storage('google-ads.yaml')
-    #ga_service = google_ads_client.get_service('GoogleAdsService', version='v3')
     response = ga_service.search_stream(customer_id, query=query)  
-    #print("len(response) = ",len(response))
     for subset in response:
         jsonObj=json_format.MessageToJson(subset)
         jsonObj=json.loads(jsonObj)  
-        #response = ga_service.search_stream(customer_id, query=query)
-        #print("len(subset) = ",len(subset))
-        print("len(jsonObj) = ",len(jsonObj))
-        #countOfSubset=3;    
-        #print("jsonObj['results'][",countOfSubset,"]['campaign']['name'] ",jsonObj["results"][countOfSubset]["campaign"]["name"])    
+        #print("len(jsonObj) = ",len(jsonObj))
         countOfSubset=0; 
         while 1000>countOfSubset:
             try:      
@@ -64,7 +58,7 @@ def fromAds(customer_id,query):
             #print("in while loop")
             except:
                return "end"
-        
+"""        
 testCampaign="150-063-1476"  
 cityAccount="210-489-7739";
 cityMobileAccount="423-859-4348";
@@ -73,6 +67,7 @@ suburbAccount="861-225-9590";
 stateAccount="644-879-0580";
 hispanicAccount="473-277-5338";
 ArrayOfAccounts=[cityAccount,cityMobileAccount,cityMobileAccount,communityAccount,suburbAccount,stateAccount,hispanicAccount]
+"""
 
 
 def accntFormat(ArrayOfAccounts):
