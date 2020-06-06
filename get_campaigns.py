724f5178df3d26e301913c728d7e75032dbe0d8f
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-06 11:25:22 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -10,6 +10,16 @@ from google.ads.google_ads.errors import GoogleAdsException
 #query = ('SELECT campaign.id, campaign.name FROM campaign ORDER BY campaign.id')
 google_ads_client = GoogleAdsClient.load_from_storage('google-ads.yaml')
 ga_service = google_ads_client.get_service('GoogleAdsService', version='v3')
+
+testCampaign="150-063-1476"  
+cityAccount="210-489-7739";
+cityMobileAccount="";
+communityAccount="";
+suburbAccount="";
+stateAccount="";
+hispanicAccount="";
+ArrayOfAccounts=[cityAccount,cityMobileAccount,cityMobileAccount,communityAccount,suburbAccount,stateAccount,hispanicAccount]
+
 query = ('SELECT campaign.id, campaign.name, campaign.status, campaign_budget.amount_micros, metrics.cost_micros, metrics.clicks,  metrics.conversions, metrics.impressions FROM campaign ORDER BY campaign.id')
 
 #966-289-6891
@@ -17,26 +27,7 @@ query = ('SELECT campaign.id, campaign.name, campaign.status, campaign_budget.am
 #customer_id=testcamp
 #response = ga_service.search_stream(customer_id, query=query)
 #countOfSubset=0;
-"""
-for subset in response:
-      print("Boundery----Boundery---")
-      #print('type(subset) ',type(subset));
-      #print('subset ',subset);
-      
-      
-      jsonObj=json_format.MessageToJson(subset)
-      jsonObj=json.loads(jsonObj)
-      #print("len(jsonObj) ",len(jsonObj))
-      #print("jsonObj['results'][",countOfSubset,"]['campaign']['name'] ",jsonObj["results"][countOfSubset]["campaign"]["name"])
-      #countOfSubset+=1;
-      #print("jsonObj['results'][",countOfSubset,"]['campaign']['name'] ",jsonObj["results"][countOfSubset]["campaign"]["name"])
-      #print("while for obj before")
-      while len(jsonObj)>countOfSubset:
-            print("jsonObj['results'][",countOfSubset,"]['campaign']['name'] ",jsonObj["results"][countOfSubset]["campaign"]["name"])
-            countOfSubset+=1
-            print("in while loop")
-      print("while for obj before")   
-"""      
+
 
 def fromAds(customer_id,query):
     print(customer_id) 
@@ -62,11 +53,32 @@ def fromAds(customer_id,query):
             except:
                return "end"
         
-testCampaign="150-063-1476"      
-fromAds(testCampaign,query); 
+testCampaign="150-063-1476"  
+cityAccount="210-489-7739";
+cityMobileAccount="";
+communityAccount="";
+suburbAccount="";
+stateAccount="";
+hispanicAccount="";
+ArrayOfAccounts=[cityAccount,cityMobileAccount,cityMobileAccount,communityAccount,suburbAccount,stateAccount,hispanicAccount]
 
-CityAccount="210-489-7739";
-fromAds(CityAccount,query);  
+"""
+fromAds(testCampaign,query); 
+fromAds(cityAccount,query); 
+fromAds(,query);
+fromAds(,query); 
+fromAds(,query); 
+fromAds(,query); 
+fromAds(,query); 
+"""
+def accntFormat(ArrayOfAccounts);
+    len(ArrayOfAccounts);
+    for accnts in ArrayOfAccounts;
+        fromAds(accnts,query);
+accntFormat(ArrayOfAccounts);      
+         
+      
+      
 
       
       
