ece702340e059da789d3d8eed0c2221956ba854b
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-07 17:59:28 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -29,7 +29,7 @@ metrics.cost_micros, metrics.clicks,  metrics.conversions, metrics.impressions F
 WHERE campaign.status="ENABLED" ORDER BY campaign.id')
 """
 
-query = ('SELECT campaign.id, campaign.name FROM campaign CAT ')
+query = ('SELECT campaign.id, campaign.name, segments.date FROM campaign  ')
 
 #966-289-6891
 
