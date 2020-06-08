257385d4d1acbc865567b7b34abfceb00bd34ac6
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-08 16:45:29 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -62,12 +62,12 @@ def fromAds(customer_id,query):
         while numberOfResults>countOfSubset:
          
             try:
-               name=jsonObj["results"][countOfSubset]["campaign"]["name"];
-               status=jsonObj["results"][countOfSubset]["campaign"]["status"];
+               name=float(jsonObj["results"][countOfSubset]["campaign"]["name"]);
+               status=float(jsonObj["results"][countOfSubset]["campaign"]["status"]);
                cost=float(jsonObj["results"][countOfSubset]["metrics"]["costMicros"])/10000;
-               clicks=jsonObj["results"][countOfSubset]["metrics"]["clicks"];
-               conversions=jsonObj["results"][countOfSubset]["metrics"]["conversions"];
-               impressions=jsonObj["results"][countOfSubset]["metrics"]["impressions"];
+               clicks=float(jsonObj["results"][countOfSubset]["metrics"]["clicks"]);
+               conversions=float(jsonObj["results"][countOfSubset]["metrics"]["conversions"]);
+               impressions=float(jsonObj["results"][countOfSubset]["metrics"]["impressions"]);
                budget=float(jsonObj["results"][countOfSubset]["campaignBudget"]["amountMicros"])/10000; 
                #print(type(cost)) 
                 
