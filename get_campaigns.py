eb4ec95aadf32b116250de8d4bf97b63204d9f32
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-08 17:50:56 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -156,16 +156,15 @@ def allAccntCombinedBasedMetrics(ArrayOfAccounts):
     
     
     #metrics=pandas.DataFrame(data=metrics)
-    metrics=" | "+str(metrics["cost"])+" | "+str(metrics["clicks"] +" | "+str(metrics["conversions"])+" | "+str(metrics["impressions"])
+    metrics=" | "+str(metrics['cost'])+" | "+str(metrics["clicks"] +" | "+str(metrics["conversions"])+" | "+str(metrics["impressions"])
                                                  
-    """
-    FinalSumOfMetrics=[sum(metrics[cost],sum(metrics[clicks]),sum(metrics[conversions])\
-                       ,sum(metrics[impressions]),sum(metrics[budget]];
+    
+    #FinalSumOfMetrics=[sum(metrics[cost],sum(metrics[clicks]),sum(metrics[conversions])\
+    #                   ,sum(metrics[impressions]),sum(metrics[budget]];
                        
     
-    FinalSumOfMetrics=pandas.DataFrame(data=FinalSumOfMetrics
-    """
-    
+    #FinalSumOfMetrics=pandas.DataFrame(data=FinalSumOfMetrics
+        
     print(metrics);
     return metrics;
     
