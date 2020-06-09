6add7585529c2d1be2afd917b26236d03bba82eb
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-08 20:16:27 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -38,7 +38,7 @@ def fromGoogleAds(customer_id,dateRange):
     
     query = ('SELECT campaign.name, campaign.status,campaign_budget.amount_micros,metrics.cost_micros,\
             metrics.clicks,metrics.conversions,metrics.impressions FROM campaign WHERE \
-            campaign.status="ENABLED" AND segments.date DURING'+dateRange+' ORDER BY campaign_budget.amount_micros')
+            campaign.status="ENABLED" AND segments.date DURING '+dateRange+' ORDER BY campaign_budget.amount_micros')
     """
     yesterdayQuery = ('SELECT campaign.name, metrics.cost_micros FROM campaign \
            WHERE campaign.status="ENABLED" AND segments.date DURING YESTERDAY')
