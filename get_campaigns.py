6cc815b94e171a42f868aa15df9a4c439a45e121
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-10 04:15:27 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -186,7 +186,7 @@ def allAccntCombinedBasedMetrics(googleArrayOfAccounts):
        #print(CampaignLevelTable)
        #CampaignLevelTable2=fromGoogleAds(accnts,"YESTERDAY");
        try:
-        mtdGoogle=fromGoogleAds(accnts,"");
+        mtdGoogle=fromGoogleAds(accnts,"20191010");
         yesterdayGoogleCost=sum(fromGoogleAds(accnts,"YESTERDAY").cost);
         cost=sum(mtdGoogle.cost);
         clicks=sum(mtdGoogle.clicks);
