d2e7ab1fd1ad0cf90c0fa6194e5c8301fcbad7f9
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-13 15:43:26 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -245,7 +245,7 @@ def perAccntCombinedBasedMetrics(accnts):
     #addComa(1000555.256);
     #addComa(10000555.256);
     #addComa(impression);
-    tryFormat=format('10000000000':,);
+    tryFormat=format('10000000000':",");
     print(tryFormat);
     """
         
