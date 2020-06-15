9abdccd1e3638c35d02ea3a63fa4c710f66abd10
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-15 08:03:52 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -220,8 +220,8 @@ def perAccntCombinedBasedMetrics(accnts):
     
     exCost=partialCost[0];
     print("exCost ",exCost);
-    #print("round(exCost,2) ",round(exCost,2));
-    round(5.2);
+    print("round(exCost,2) ",round(exCost,2));
+    #round(5.2);
     
     metrics={"Accnt Name":["Google "+AccntName+" Account MTY"],"cost":partialCost,"clicks":partialClicks,"conversions":partialConversions\
              ,"impressions":partialImpressions,"CPC":CPC,"CPL":CPL,"Conv. rate":ConvRate,"CTR":CTR\
