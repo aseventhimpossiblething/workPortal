2e7061758aa43b96adae73e38ffdfa623574e211
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-17 08:21:02 -0500

    Create bingEXP.py

---
+++ bingEXP.py
@@ -0,0 +1,7 @@
+campaign_service = ServiceClient(
+    service='CampaignManagementService', 
+    version = 13,
+    authorization_data=authorization_data, 
+    environment = ENVIRONMENT,
+)
+print campaign_service.soap_client
