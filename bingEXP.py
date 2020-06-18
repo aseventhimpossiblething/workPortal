6acd4cf9ca74dcce34f8a7b29fbf468262dd4050
aseventhimpossiblething <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-06-18 11:22:10 -0500

    Update bingEXP.py

--- bingEXP.py
+++ bingEXP.py
@@ -7,7 +7,7 @@ print("Bing  Loading")
 campaign_service = ServiceClient(
     service='CampaignManagementService', 
     version = 13,
-    authorization_data=authorization_data, 
+    #authorization_data=authorization_data, 
     environment = ENVIRONMENT,
 )
 print(campaign_service.soap_client)
