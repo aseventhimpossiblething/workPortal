f9ebf3a25f972e87e2ffa7ea63f8ef317e0fac9e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-08-28 09:27:30 -0500

    Update get_campaigns.py

--- get_campaigns.py
+++ get_campaigns.py
@@ -434,9 +434,17 @@ def fromGoogleAds(customer_id,dateRange):
         campaign=jsonObj["results"][countOfSubset]['campaign'];
         adGroup=jsonObj["results"][countOfSubset]['adGroup'];
         keyword=jsonObj["results"][countOfSubset]['adGroupCriterion'];
+        cost=jsonObj["results"][countOfSubset]['metrics']['cost']
+        clicks=jsonObj["results"][countOfSubset]['metrics']['clicks']
+        conversions=jsonObj["results"][countOfSubset]['metrics']['conversions']
+        impressions=jsonObj["results"][countOfSubset]['metrics']['impressions']
         print(campaign);
         print(adGroup);
         print(adGroupCriterion);
+        print(cost);
+        print(clicks);
+        print(conversions;
+        print(impressions);      
      
         #return 0;
          
