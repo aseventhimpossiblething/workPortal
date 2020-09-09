8881306ad3637586ed9f6b58c8710f34926d944a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-09-09 07:24:44 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -413,7 +413,12 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    Campaign_Name.append(Campaign_Nameing_Conv);
    
    communityName=str(NewDataFrame['Community Name'][count]);
-   communityName.replace("40s","").replace("40'","").replace("40","").replace("45s","")
+   communityName.replace("40s","").replace("40'","").replace("40","").replace("45s","")\
+                .replace("45'","").replace("45","").replace("50s","").replace("50'","").replace("50","").replace("55s","")\
+                .replace("55'","").replace("55","").replace("60s","").replace("60'","").replace("60","").replace("65s","")\
+                .replace("65'","").replace("65","").replace("70s","").replace("70'","").replace("70","").replace("75s","")\
+                .replace("75'","").replace("75","").replace("80s","").replace("80'","").replace("80","").replace("85s","")\
+                .replace("85'","").replace("85","").replace("90s","").replace("90'","").replace("90","").replace("95s","")\ 
       
    """ 
       .replace("40s","").replace("40'","").replace("40","").replace("45s","")\
