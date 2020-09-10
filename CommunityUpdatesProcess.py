d36de479096ac9bd3057280648a0aee3f4d72f45
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-09-10 15:31:13 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -826,7 +826,7 @@ def initialCommUpdatProcess():
            thisLoopCount+=1;
      DedupedMultiples=list(dict.fromkeys(Multiples));
      DedupedCleanUP=cleanupFrame.drop(cleanupFrame.index[repeatedRows]);
-     MPCFrame=cleanupFrame.iloc[repeatedRows].drop_Duplicates();
+     MPCFrame=cleanupFrame.iloc[repeatedRows].drop_duplicates();
      
      
      print("MPCFrame ",MPCFrame);
