e6cad7e94b64060df1cc36430a8e2edb76f231a3
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-09-12 07:20:31 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -924,9 +924,9 @@ def initialCommUpdatProcess():
          NewUrls=[] 
          cntr=0;
          while cntr<len(topCleaned['Community Name']):
-               urlsAssembly='https://www.newhomesource.com/community/'+topCleaned['State']\
-                          [cntr]+"/"+topCleaned['City'][cntr].replace(" ","-")+"/"+topCleaned['Community Name']\
-                          [cntr].replace(" ","-")+"-by-"+topCleaned['Brand Name'][cntr].replace(" ","-")+"/"
+               urlsAssembly='https://www.newhomesource.com/community/'+topCleaned['State'][cntr]+"/"+topCleaned['City'][cntr]
+                          #.replace(" ","-")+"/"+topCleaned['Community Name']\
+                          #[cntr].replace(" ","-")+"-by-"+topCleaned['Brand Name'][cntr].replace(" ","-")+"/"
                           #+str(topCleaned['Community Id'][cntr])
                           #+"?refer=")\
                           #.lower() ;
