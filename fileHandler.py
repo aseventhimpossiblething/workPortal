414a6e604a1df093a6f46d2c7560ac5e2eb4c104
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-09-13 08:21:52 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -47,8 +47,6 @@ def googConverter(X):
     print("GoogConverter Running")    
     Temp=X;
     cols=Temp.columns
-    #print("initial cols ",cols)
-    #"Impr. (Top) %"    
     New_cols=[];
     for col in cols:
         col=str(col).replace("Impr. (Abs. Top) %","Absolute Top Impression Share").replace("Impr. (Top) %","Top Impr. share").replace("New CPC","New Bid").replace("Cost / conv.","CPA").replace("'","").replace('Max. CPC','Bid').replace('Cost','Spend')\
@@ -74,29 +72,11 @@ def BidOpFileHandler():
     print("Temp 1")
     print(Temp)
     record_async_start=open("ForestLoadingQueue.txt","w")
-    #print("async open and read")    
-    #print(record_async_start.read())
     record_async_start.write("This should take no more than 5 min.. else resubmit form")
     record_async_start.close()
-    #record_async_start=open("ForestLoadingQueue.txt","r")
-    #record_async_start.close()
-    #rowcheck(,) 
-  
+      
     target_Variable='New Bid' 
-    #rowcheck(,target_Variable)    
-        
-    """
-    print("target_Variable Defined")    
-    print("target_Variable")  
-    print(target_Variable) 
-    """
-        
-    #designated_Columns=['Campaign','Ad group','Match type',target_Variable,'Bid','Avg. CPC','CPA']         
-    #core_cols=['Campaign','Ad group',target_Variable,'Match Number','Bid','Avg. CPC','CPA']     
-       
-        
-        
-        
+           
     designated_Columns=['Campaign','Ad group','Keyword','Impr.','Match type',target_Variable,'Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank']         
     core_cols=['Campaign','Ad group','Impr.',target_Variable,'Match Number','Market Number','Bid','Clicks','CTR','Avg. CPC','Spend','Conv.','CPA','Conv. rate','Top Impr. share','Absolute Top Impression Share','Impr. share (IS)','Qual. score','IS lost to rank']     
     
@@ -105,16 +85,8 @@ def BidOpFileHandler():
     isGoog1=str(Temp.columns).find('Cost')
     isGoog2=str(Temp.columns).find('Conversions')
     Temp=googConverter(Temp)
-    #print(Temp.columns)
-    
-       
-    #print("Temp['Match type']",Temp['Match type'])
-    #Temp=pandas.DataFrame(Temp,columns=designated_Columns);
-    #print("Temp['Match type']",Temp['Match type'])
-    
-    #print("str(Temp.columns).find(New Bid)",str(Temp.columns).find('New Bid')) 
+  
     isTrainingSheet=str(Temp.columns).find('Change'); 
-    #if isTrainingSheet=="Xongo":    
     if isTrainingSheet!=-1:
       
        def TrainingSheetBehavior(x,x2,Temp):
@@ -123,15 +95,9 @@ def BidOpFileHandler():
            designated_Columns=x
            core_cols=x2   
            Temp=Temp     
-          
-                      
            os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
-           #Temp=pandas.read_excel('Temp.xlsx')
-          
-         
-           #Temp=googConverter(Temp)     
+                
            rowCheck=rowcheck(Temp,designated_Columns)     
-           #print(len(rowCheck)," ",rowCheck); 
            if len(rowCheck)>0:
                 os.chdir('/var/www/workPortal/Sheets/BidOpData/MachinePatternSheets/')
                 rowCheck=str(rowCheck)
@@ -140,38 +106,30 @@ def BidOpFileHandler():
                 record_async_start.close()
                 rowCheck=" The following Columns are missing "+rowCheck+" please resubmit sheet "
                 return rowCheck
-                
-           
            Temp=pandas.DataFrame(Temp,columns=designated_Columns)
            Temp.fillna(0)
-                                       
            record_async_start=open("ForestLoadingQueue.txt","w+")
            record_async_start.write("15%")
            record_async_start.close() 
-          
            Temp['Match Number']=BidOpAssist.Match_num(Temp);
-         
-           
+                   
            Temp['Market Number']=BidOpAssist.MarketNumberGen(Temp)
            core=pandas.read_excel('BidOpSeed.xlsx')
            core=core.append(Temp, sort='False')
            core=pandas.DataFrame(core,columns=core_cols)     
            core.to_excel("BidOpSeed.xlsx")
-       
-                
+                       
            record_async_start=open("ForestLoadingQueue.txt","w")
            record_async_start.write("100%")
            record_async_start.close();  
            
            print("Temp 2")
            print(Temp)
-                
-           
+                     
            return "<html><a href='/BasisOfBids'>This Training Sheet will be added to the body of training Data Click to view Basis Sheet</a></html>"
-      
        TrainLoad=threading.Thread(target=TrainingSheetBehavior, args=[designated_Columns, core_cols,Temp]);
        TrainLoad.start(); 
-       #print("is there an attempt to return")
+      
        return "<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL=/BidOpPending'><html>did not forward</html>"
        
     else:
@@ -193,10 +151,7 @@ def BidOpFileHandler():
                 record_async_start.close()
                 rowCheck=" The following Columns are missing "+rowCheck+" please resubmit sheet "
                 return rowCheck
-                     
-       #print("Temp from fileHandler");
-       #print("Temp.columns from fileHandler ",Temp.columns.values);
-       #print("*** Temp['Match type'] in else before BidOp Insert *** ",Temp['Match type']); 
+              
        print("Just Before threading.thread")
        BidOpAssistAsync=threading.Thread(target=BidOpAssist.BidOpOverview,args=[designated_Columns,core_cols,target_Variable,Temp]);
        BidOpAssistAsync.start(); 
@@ -205,7 +160,6 @@ def BidOpFileHandler():
         
     
     toscrn=isTrainingSheet
-    
     return toscrn
   
 def CommListFileHandler():
@@ -219,9 +173,7 @@ def CommListFileHandler():
         return "Google slot is empty"
     if emptyObj==str(request.files['Communities']):
         return "Active Community slot is empty"
-       
     
-    #print("did not Check That File Slots are Filled")    
     
     if request.files['Communities'].filename.find("xlsx")<1:
                 return "The Community Sheet is not XLSX file type";
@@ -229,27 +181,19 @@ def CommListFileHandler():
                 return "The Google Sheet is not XLSX file type";
     if request.files['currentBing'].filename.find("xlsx")<1:
                 return "The Bing Sheet is not XLSX file type"; 
-            
-    #print("did not Check that Filles are XlSX")    
-   
+     
     os.chdir(currentCommunitiesLocation)
     SHcommand="sudo chmod -R 777 "+currentCommunitiesLocation
     os.system(SHcommand+"/WorkingCommunities")
-    #os.system(SHcommand)
     request.files['Communities'].save('WorkingCommunities')
-    #os.system(SHcommand+"/WorkingCommunities")
-       
+        
     os.chdir(currentGoogleLocation)
     request.files['currentGoogle'].save('WorkingGoogle')
     
     os.chdir(currentBingLocation)
     request.files['currentBing'].save('WorkingBing')
      
-    #print("saved Files") 
-  
-    
-   
-   
+ 
        
     def async_fileloader():
      os.chdir(SheetsFileLocation) 
@@ -259,9 +203,7 @@ def CommListFileHandler():
      CommunityUpdatesProcess.initialCommUpdatProcess()
     LoadAllCommunityFiles=threading.Thread(target=async_fileloader)
     LoadAllCommunityFiles.start()    
-    #print("Should respond now!!! ")
-    #os.system('echo his is Echoed by system os number 3')    
-    #return "<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL=http://bhiapilink.com/DisplayCommUpdate'><html>did not forward</html>"
+
     return "<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL=/DisplayCommUpdate'><html>did not forward</html>"     
 
 
