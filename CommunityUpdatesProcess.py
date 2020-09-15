40e60f820a58c99bb365569c85f39c750e932b99
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-09-15 06:22:45 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -704,9 +704,11 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    Path1A.append(Path1A_conv)
    Path2A.append("New Homes")
    #print("NewDataFrame['URL Signal'][count]" ,NewDataFrame['URL Signal'][count]) 
+   """  
    if NewDataFrame['URL Signal'][count]!="Default":
       #print("NewDataFrame['URL Signal'][count]" ,NewDataFrame['URL Signal'][count])
       URL_Struct1=NewDataFrame['URL Signal'][count];      
+   """
    Final_URL.append(URL_Struct1)
  
   
