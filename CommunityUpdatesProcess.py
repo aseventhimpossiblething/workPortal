025548ea1f4c7048dbc33f93d4b97297fdc41db1
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-11-27 15:19:17 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -515,9 +515,8 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
             .replace(" ","-")+"/"+str(NewDataFrame['Community Id'][count])+"?refer=").lower().replace("m/i","m-i");
   """          
 
-  URL_Struct1=str("https://www.newhomesource.com/community/"\
-            +NewDataFrame['State'][count]+"/"+NewDataFrame['City'][count].replace(" ","-")+"/"+communityName.replace(" ","-")+"-by-"+NewDataFrame['Brand Name'][count].replace(" ","-")+"/"+str(NewDataFrame['Community Id'][count])+"?refer=").lower().replace("m/i","m-i");
-      
+  #URL_Struct1=NewDataFrame['Community Id'][count]+"?refer=").lower().replace("m/i","m-i");
+  URL_Struct1=str("https://www.newhomesource.com/community/"+NewDataFrame['State'][count]+"/"+NewDataFrame['City'][count].replace(" ","-")+"/"+ communityName.replace(" ","-")+"-by-"+NewDataFrame['Brand Name'][count].replace(" ","-")+"/"+str(NewDataFrame['Community Id'][count]))  
   print("Leaving 3rd Replace Complex! URL Struct line 517")
   print("Entering 4th Replace Complex! URLstruct line 518")
   URL_Struct1=URL_Struct1.replace("'","").replace("m/s","m-s").replace("---","-").replace("--","-")\
