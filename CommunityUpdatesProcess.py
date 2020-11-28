1581c87ebe3bb03e425f52a3ffded1626224cc0e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-11-28 00:08:02 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -539,8 +539,9 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
             .replace(" ","-")+"/"+str(NewDataFrame['Community Id'][count])+"?refer=").lower().replace("m/i","m-i");
   """         
   print("str(NewDataFrame['Community Id'][count]) ",str(NewDataFrame['Community Id'][count]))
-  URL_Struct1="https://www.newhomesource.com/community/"+NewDataFrame['State'][count]+"/"+NewDataFrame['City'][count]+"/"+communityName
-            #+"-by-"+NewDataFrame['Brand Name'][count]+"/"
+  URL_Struct1="https://www.newhomesource.com/community/"+NewDataFrame['State'][count]+"/"+NewDataFrame['City'][count]+"/"+communityName\
+            +"-by-"
+            #+NewDataFrame['Brand Name'][count]+"/"
             #+str(NewDataFrame['Community Id'][count])
             #+str(NewDataFrame['Community Id'][count])
             #+"?refer="
