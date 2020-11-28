4f3741cd59c4f9d3f368174f3551db5208fdeea5
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2020-11-27 22:11:25 -0600

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -555,7 +555,7 @@ def KeywordGen(NewDataFrame,MatchType,SearchChan):
    if SearchChan=="google":
     URL_Struct1=URL_Struct1+"gppc"
     #Campaign_Nameing_Conv=Market_LookUp.google[NewDataFrame['Market ID'][count]]
-    Campaign_Nameing_Conv=NewDataFrame['Market ID'][count]  
+    Campaign_Nameing_Conv=NewDataFrame['Market Name'][count]  
     Campaign_Nameing_Conv=Campaign_Nameing_Conv.replace("SBMM",MatchType)
     if MatchType=="SBMM":
      URL_Struct1=URL_Struct1+"403"
