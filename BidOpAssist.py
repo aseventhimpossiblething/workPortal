59283b5b9a4e98202047cfc3561fa9c0fc87a13c
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-04-02 17:18:12 -0500

    Update BidOpAssist.py

--- BidOpAssist.py
+++ BidOpAssist.py
@@ -165,7 +165,9 @@ def CTROverview(desiCols,corecols,change,Temp):
     .fillna(0).replace(" --",0).fillna(0).replace("< 10%",10).fillna(0).replace("> 90%",90).fillna(0);
     XofSeed=Seed.drop(['Campaign','Ad group'],axis=1);
     YofSeed=Seed[PredVar];
-    print(" XofSeed - ",XofSeed)
+    print(" XofSeed - ")
+    print(XofSeed)
+    print(XofSeed[PredVar])
     
     #ImpressionMetricXofSeed=Seed.drop(['Campaign','Ad group',PredVar],axis=1);
     #ImpressionMetricYofSeed=Seed[PredVar];
