2f04268fbeb41d5cb13f490f203d05b66b68ffe5
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-04-03 20:02:18 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -77,14 +77,14 @@ def CTRUploadFilehandler():
       
     target_Variable='CTR' 
            
-    designated_Columns=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
-    core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
+    #designated_Columns=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
+    @core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
     #core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Absolute Top Impression Share','Impr. share (IS)']  
     #core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
     #designated_Columns=['Search top IS','Search abs. top IS','Search impr. share']
     
-    #designated_Columns=['Campaign','Ad group',target_Variable,'Search top IS','Search abs. top IS','Search impr. share']  
-    #core_cols=['Campaign','Ad group',target_Variable,'Search top IS','Search abs. top IS','Search impr. share']      
+    designated_Columns=['Campaign','Ad group',target_Variable,'Search top IS','Search abs. top IS','Search impr. share']  
+    core_cols=['Campaign','Ad group',target_Variable,'Search top IS','Search abs. top IS','Search impr. share']      
         
         
     print('target_Variable',target_Variable);
