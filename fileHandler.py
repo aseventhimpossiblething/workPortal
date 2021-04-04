f3c106082696f611ac9fb45dc21edd375a3667cf
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-04-03 20:04:30 -0500

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -78,7 +78,7 @@ def CTRUploadFilehandler():
     target_Variable='CTR' 
            
     #designated_Columns=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
-    @core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
+    #core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
     #core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Absolute Top Impression Share','Impr. share (IS)']  
     #core_cols=['Campaign','Ad group','Impr.',target_Variable,'Clicks','Cost','Search top IS','Search abs. top IS','Search impr. share']  
     #designated_Columns=['Search top IS','Search abs. top IS','Search impr. share']
