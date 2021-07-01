e7e2b984e63c17302cba852ddba8c992682c1779
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-01 07:19:33 -0500

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -2,4 +2,4 @@
 sleep 5
 cd /GMDelight/workPortal
 #gunicorn -c appconfig.py bdx-api-link:app
-gunicorn --bind=127.0.0.1:8000 bdx-api-link:app
+gunicorn --bind=127.0.0.1:8000 bdx-api-link:app --daemon
