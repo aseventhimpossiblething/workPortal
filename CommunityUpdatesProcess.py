87da9d486a33787ef1a1ed810754b626fb5604ef
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-08-12 19:02:07 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -877,7 +877,7 @@ def KeywordGenII(NewDataFrame,SearchChan):
  BingAdFrameB=pandas.DataFrame(BingAdFrameB).drop_duplicates()
       
       
- print(GoogleKWFrame)
+ print(GoogleKWFrame["Campaign Name","Ad Group"])
  
 
  if SearchChan=="google":
