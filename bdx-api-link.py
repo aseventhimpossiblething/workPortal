e30f22ddcad67af26bd830ebd53edb6bca28188c
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-01 17:06:56 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -452,7 +452,7 @@ def CommunityFileHandling():
 @app.route('/NovelCommunityFileHander', methods=['POST','GET'])
 def NCommunityFileHandling():
     print("NCommunityFileHandling()")
-    return fileHandler.CommListFileHandler()
+    return fileHandler.NCommListFileHandler()
     
 
 @app.route('/account')
