1616f4dfbcf9216f8495f2a5b9eca57fa66cce6a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-24 11:56:20 -0500

    Update CommunityUpdatesProcess2.py

--- CommunityUpdatesProcess2.py
+++ CommunityUpdatesProcess2.py
@@ -913,8 +913,8 @@ def KeywordGenII(NewDataFrame,SearchChan):
  print("Making DataSheets-----------------------------------------------------------------------------------------")  
  GoogleKWFrame={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Keyword":Keyword,"Match type":Match_Type,"Status":Status,"Max CPC":Bid,"Labels":KWLabel} 
  GoogleKWFrame=pandas.DataFrame(GoogleKWFrame)
- GoogleAdFrameA={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Headline 1":Title1A,"Headline 2":Title2A,"Headline 3--":Title3A,\
-                "Headline 4":Title4A,"Description":TextA,"Description 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":Label}
+ GoogleAdFrameA={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Headline 1":Title1A,"Headline 2":Title2A,"Headline 3":Title3A,\
+                "Description":TextA,"Description 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":Label}
  GoogleAdFrameB={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Headline 1":Title1A,"Headline 2":Title2A,"Headline 3":Title3A,\
                 "Description":TextB,"Description 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":LabelB}
  GoogleAdFrameA=pandas.DataFrame(GoogleAdFrameA).drop_duplicates()
