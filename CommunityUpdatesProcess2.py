a6c3ca5d72c73f4a4072b96e5dc6ca098e7f9d5a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-31 13:54:34 -0500

    Update CommunityUpdatesProcess2.py

--- CommunityUpdatesProcess2.py
+++ CommunityUpdatesProcess2.py
@@ -1295,7 +1295,7 @@ def initialCommUpdatProcess():
      commIDstr=str(y['CommunityID']);
      count=0;
      while count<len(x[CommunityID]):
-           comNumInMain=x[CommunityID][count];
+           comNumInMain=str(x[CommunityID][count]);
            comNumInAttributesrow=str(y['CommunityID'][count+4]); 
            print("comNumInMain=",comNumInMain," count=",count," comNumInAttributesrow=",comNumInAttributesrow)
            comIDpositionInAttributes=commIDstr.find(comNumInMain)
