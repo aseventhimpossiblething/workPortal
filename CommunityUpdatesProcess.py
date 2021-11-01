b0089f046ba078b90738e40c85cfff857fc50e75
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-11-01 16:54:32 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -440,24 +440,61 @@ def KeywordGenII(NewDataFrame,SearchChan):
  Campaign_Name=[];
  Adgroup=[];
  Keyword=[];
+ Adtype=[];     
  Match_Type=[];
  Status=[];
  Bid=[];
  Final_URL=[];
+      
+      
 
+ hl1pos=[];
+ hl2pos=[]; 
+      
  Title1A=[];
  Title2A=[];
  Title3A=[];
+ Title4A=[];
+ Title5A=[]; 
+ Title6A=[];
+ Title7A=[];
+ Title8A=[];
+ Title9A=[];
+ Title10A=[]; 
+ Title11A=[];
+ Title12A=[];
+ Title13A=[];
+ Title14A=[];
+ Title15A=[];
+
  TextA=[];
  Text2A=[];
+ Text3A=[];
+ Text4A=[];
+ 
  Path1A=[];
  Path2A=[];
  
  Title1B=[];
  Title2B=[];
  Title3B=[];
+ Title4B=[];
+ Title5B=[]; 
+ Title6B=[];
+ Title7B=[];
+ Title8B=[];
+ Title9B=[];
+ Title10B=[]; 
+ Title11B=[];
+ Title12B=[];
+ Title13B=[];
+ Title14B=[];
+ Title15B=[];
+      
  TextB=[];
  Text2B=[];
+ Text3B=[];
+ Text4B=[];     
  Path1B=[];
  Path2B=[];
  
@@ -672,6 +709,20 @@ def KeywordGenII(NewDataFrame,SearchChan):
    Status.append("Active")
    Status.append("Active")
    Status.append("Active")
+
+   hl1pos.append(1);  
+   hl2pos.append(2);
+   hl1pos.append(1);  
+   hl2pos.append(2); 
+   hl1pos.append(1);  
+   hl2pos.append(2);
+   RSALabel.append("RSA Ads");
+   RSALabel.append("RSA Ads");  
+   RSALabel.append("RSA Ads");      
+   Adtype.append("Responsive search ad");  
+   Adtype.append("Responsive search ad");
+   Adtype.append("Responsive search ad");
+   
    
 
    Title1A_Name_Conv=communityName
@@ -695,10 +746,97 @@ def KeywordGenII(NewDataFrame,SearchChan):
    Title2A.append(Title2A_conv)
    Title2A.append(Title2A_conv)
    Title2A.append(Title2A_conv)
-        
+   """     
    Title3A.append("Schedule a new home tour today")
    Title3A.append("Schedule a new home tour today")
    Title3A.append("Schedule a new home tour today")
+   """
+   Title3ACopy="Schedule a new home tour today"
+   #if NewDataFrame['Condo OR TownHome'][count]==1 or NewDataFrame['With At Least One MultiFamily Plan'][count]==1:
+   #   Title3ACopy="Multi-Family Floor Plans"
+   Title3A.append(Title3ACopy)
+   Title3A.append(Title3ACopy)
+   Title3A.append(Title3ACopy)
+   
+   Title4ACopy="View Move-in Ready Homes"
+   #if NewDataFrame['Active Adult'][count]==1:
+   #   Title4ACopy="55+ Active Adult Living"
+   Title4A.append(Title4ACopy);
+   Title4A.append(Title4ACopy);
+   Title4A.append(Title4ACopy);
+
+   Title5ACopy="Your Dream Home Awaits"
+   #if NewDataFrame['Has A Luxury Home'][count]==1:
+   #   Title5ACopy="Luxurious Features Available"   
+   Title5A.append(Title5ACopy); 
+   Title5A.append(Title5ACopy);
+   Title5A.append(Title5ACopy);
+      
+   Title6ACopy="Your New Home is Waiting"
+   #if NewDataFrame['Gated'][count]==1:
+   #   Title6ACopy="Enjoy Safe Gated Communities"  
+   Title6A.append(Title6ACopy);
+   Title6A.append(Title6ACopy);
+   Title6A.append(Title6ACopy);
+   
+   Title7ACopy="Finest Location & Amenities"
+   #if NewDataFrame['Pool'][count]==1:
+   #   Title7ACopy="Beautiful Resort-Style Pools"
+   Title7A.append(Title7ACopy);
+   Title7A.append(Title7ACopy);
+   Title7A.append(Title7ACopy);
+
+   Title8ACopy="Move in Ready Homes Available! "
+   #if NewDataFrame['Green'][count]==1:
+   #   Title7ACopy="Energy-Efficient & Eco-Smart"   
+   Title8A.append(Title8ACopy);
+   Title8A.append(Title8ACopy);
+   Title8A.append(Title8ACopy);   
+
+
+   Title9ACopy="------------"
+   #if NewDataFrame['Parks'][count]==1 or NewDataFrame['Nature'][count]==1:
+   #   Title7ACopy="Access to Parks and Green Spaces"      
+   Title9A.append(Title9ACopy);
+   Title9A.append(Title9ACopy);
+   Title9A.append(Title9ACopy);
+
+   Title10ACopy="- 10 no default " 
+   Title10A.append(Title10ACopy);
+   Title10A.append(Title10ACopy);
+   Title10A.append(Title10ACopy);   
+
+   Title11ACopy="- 11 no default "  
+   Title11A.append(Title11ACopy);
+   Title11A.append(Title11ACopy);
+   Title11A.append(Title11ACopy);
+
+   Title12ACopy="- 12 no default " 
+   Title12A.append(Title12ACopy);
+   Title12A.append(Title12ACopy);
+   Title12A.append(Title12ACopy);   
+
+   Title13ACopy="- 13 no default " 
+   Title13A.append(Title13ACopy);
+   Title13A.append(Title13ACopy);
+   Title13A.append(Title13ACopy);   
+
+   Title14ACopy="- 14 no default "
+   Title14A.append(Title14ACopy);
+   Title14A.append(Title14ACopy);
+   Title14A.append(Title14ACopy);   
+
+   Title15ACopy="- 15 no default "
+   Title15A.append(Title15ACopy);
+   Title15A.append(Title15ACopy);
+   Title15A.append(Title15ACopy);   
+    
+
+
+
+
+
+
 
    PreTextA="Find your family a perfect new home at "+str(communityName)+" in "+str(NewDataFrame['City'][count])\
       +", "+str(NewDataFrame['State'][count])
@@ -725,6 +863,14 @@ def KeywordGenII(NewDataFrame,SearchChan):
    Text2A.append("New Homes offer security, energy efficiency, and peace of mind. Skip the remodel, Buy New!")
    Text2A.append("New Homes offer security, energy efficiency, and peace of mind. Skip the remodel, Buy New!")
    Text2A.append("New Homes offer security, energy efficiency, and peace of mind. Skip the remodel, Buy New!")
+
+   Text3A.append("Get connected with trusted builders to see more floor plans & offers of your future home.");
+   Text3A.append("Get connected with trusted builders to see more floor plans & offers of your future home.");
+   Text3A.append("Get connected with trusted builders to see more floor plans & offers of your future home.");   
+    
+   Text4A.append("Save more money by buying a newly constructed home and enjoy a lower cost of ownership!");
+   Text4A.append("Save more money by buying a newly constructed home and enjoy a lower cost of ownership!");
+   Text4A.append("Save more money by buying a newly constructed home and enjoy a lower cost of ownership!");   
    
 
    Path1A_conv=NewDataFrame['City'][count].replace(" ","-")
@@ -866,16 +1012,26 @@ def KeywordGenII(NewDataFrame,SearchChan):
                 "Description":TextA,"Description 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":Label}
  GoogleAdFrameB={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Headline 1":Title1A,"Headline 2":Title2A,"Headline 3":Title3A,\
                 "Description":TextB,"Description 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":LabelB}
+ GoogleAdFrameRSA={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Ad type":Adtype,"Labels":RSALabel,"Headline 1":Title1A,"Headline 1 position":hl1pos,"Headline 2":Title2A,"Headline 2 position":hl2pos,"Headline 3":Title3A,\
+                 "Headline 4":Title4A,"Headline 5":Title5A,"Headline 6":Title6A,"Headline 7":Title7A,"Headline 8":Title8A,"Headline 9":Title9A,\
+                 "Description":TextA,"Description 2":Text2A,"Description 3":Text3A,"Description 4":Text4A,\
+                 "Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status}     
  GoogleAdFrameA=pandas.DataFrame(GoogleAdFrameA).drop_duplicates()
  GoogleAdFrameB=pandas.DataFrame(GoogleAdFrameB).drop_duplicates()
+ GoogleAdFrameRSA=pandas.DataFrame(GoogleAdFrameRSA).drop_duplicates()  
  BingKWFrame={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Keyword":Keyword,"Match type":Match_Type,"Status":Status,"Bid":Bid,"Labels":KWLabel} 
  BingAdFrameA={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Title Part 1":Title1A,"Title Part 2":Title2A,"Title Part 3":Title3A,\
                 "Text":TextA,"Text Part 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":Label}
  BingAdFrameB={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Title Part 1":Title1A,"Title Part 2":Title2A,"Title Part 3":Title3A,\
                 "Text":TextB,"Text Part 2":Text2A,"Path 1":Path1A,"Path 2":Path2A,"Final URL":Final_URL,"Status":Status,"Labels":LabelB}
+ BingAdFrameRSA={"Campaign Name":Campaign_Name,"Ad Group":Adgroup,"Title Part 1":Title1A,"Title Part 2":Title2A,"Title Part 3":Title3A,\
+                "Title Part 4":Title4A,"Title Part 5":Title5A,"Title Part 6":Title6A,"Title Part 7":Title7A,"Title Part 8":Title8A,"Title Part 9":Title9A,\
+                "Text":TextA,"Text Part 2":Text2A,"Text Part 3":Text3A,"Text Part 4":Text4A,"Path 1":Path1A,"Path 2":Path2A,\
+                "Final URL":Final_URL,"Status":Status,"Labels":Label}
  BingKWFrame=pandas.DataFrame(BingKWFrame)
  BingAdFrameA=pandas.DataFrame(BingAdFrameA).drop_duplicates()
  BingAdFrameB=pandas.DataFrame(BingAdFrameB).drop_duplicates()
+ BingAdFrameRSA=pandas.DataFrame(BingAdFrameRSA).drop_duplicates() 
       
  
  #GoogleKWFrame=GoogleKWFrame[["Campaign Name","Ad Group"]]
