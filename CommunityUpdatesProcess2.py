9322172367480fe0789b61327099731ac49d2f1a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-31 22:50:12 -0500

    Update CommunityUpdatesProcess2.py

--- CommunityUpdatesProcess2.py
+++ CommunityUpdatesProcess2.py
@@ -724,28 +724,36 @@ def KeywordGenII(NewDataFrame,SearchChan):
 
    Title5ACopy="Your Dream Home Awaits"
    if NewDataFrame['Has A Luxury Home'][count]==1:
-      Title5ACopy="Luxurious Features Available"   
+      Title5ACopy="Luxurious Homes Available"   
    Title5A.append(Title5ACopy); 
    Title5A.append(Title5ACopy);
    Title5A.append(Title5ACopy);
       
-   Title6ACopy="Join in the Interest List"   
+   Title6ACopy="Your New Home is Waiting"
+   if NewDataFrame['Gated'][count]==1:
+      Title6ACopy="Enjoy Safe Gated Communities"  
    Title6A.append(Title6ACopy);
    Title6A.append(Title6ACopy);
    Title6A.append(Title6ACopy);
    
    Title7ACopy="Finest Location & Amenities"
+   if NewDataFrame['Pool'][count]==1:
+      Title7ACopy="Beautiful Resort-Style Pools"
    Title7A.append(Title7ACopy);
    Title7A.append(Title7ACopy);
    Title7A.append(Title7ACopy);
 
-   Title8ACopy="- 8 no default "
+   Title8ACopy="Move in Ready Homes Available! "
+   if NewDataFrame['Green'][count]==1:
+      Title7ACopy="Energy-Efficient & Eco-Smart"   
    Title8A.append(Title8ACopy);
    Title8A.append(Title8ACopy);
    Title8A.append(Title8ACopy);   
 
 
-   Title9ACopy="- 9 no default " 
+   Title9ACopy="- 9 no default "
+   if NewDataFrame['Parks'][count]==1 or NewDataFrame['Nature'][count]==1:
+      Title7ACopy="Access to Parks and Green Spaces"      
    Title9A.append(Title9ACopy);
    Title9A.append(Title9ACopy);
    Title9A.append(Title9ACopy);
