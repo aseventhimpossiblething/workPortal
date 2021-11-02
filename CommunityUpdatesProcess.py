ed19e0d03574436a6b21089c6869100d38f01387
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-11-02 05:06:05 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -131,7 +131,7 @@ def WorkingBing():
  print(os.listdir())
  #try:
  WorkingBing=pandas.read_excel('WorkingBing')
- IsBingValid=CheckSheetData("WorkingBing",WorkingBing,'Campaign','Ad Group','Final Url','Final Url')
+ IsBingValid=CheckSheetData("WorkingBing",WorkingBing,'Campaign','Ad Group','Final Url','Title Part 1')
  """
  except:
     print("BING SHEET REPLACED WITH GOOGLE SHEET!!====BING SHEET REPLACED WITH GOOGLE SHEET!!==BING SHEET REPLACED WITH GOOGLE SHEET!!====BING SHEET REPLACED WITH GOOGLE SHEET!!") 
