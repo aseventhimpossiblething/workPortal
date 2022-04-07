efa85a39b9900c7e48c8be12674c2eefcb9dddb7
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-04-07 11:56:34 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -1481,15 +1481,16 @@ def initialCommUpdatProcess():
 
  WorkingCommunities['Community ID'];
  WorkingGoogleEOF['Final URL']; 
-
+ """
  WorkingBingEOFCaps=str(WorkingBingEOF.iloc[0]).find('Final URL');
  print("FInal URL caps? 1=y ",WorkingBingEOFCaps);     
  print(WorkingBingEOF.iloc[0]);
  if WorkingBingEOFCaps==1:
     WorkingBingEOF['Final Url']=WorkingBingEOF['Final URL']; 
+ """
  WorkingBingEOF['Final Url'];
-      
-  
+ print("WorkingBingEOF['Final Url'];")     
+ print(WorkingBingEOF['Final Url'])  
    
  
 
