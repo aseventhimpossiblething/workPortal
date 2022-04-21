8d3bc6f8e801ece12e375c88bc768c343cd6c59d
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-04-21 09:14:21 -0500

    Update CommunityUpdatesProcess.py

--- CommunityUpdatesProcess.py
+++ CommunityUpdatesProcess.py
@@ -1569,6 +1569,7 @@ def initialCommUpdatProcess():
  #bingURLS=MergeURLs(WorkingBingEOF['Final Url'],"Bing")+"bingETAstart"+MergeURLs(WorkingBingETA(),"Bing2");
  #googleURLS=MergeURLs(WorkingGoogleEOF['Final URL'],"Google");
  #bingURLS=MergeURLs(WorkingBingEOF['Final Url'],"Bing");
+ print("googleURLS = ",googleURLS);     
  print("bingURLS = ",bingURLS);     
       
  WorkingCommunities=filterNonParticipators(WorkingCommunities);
